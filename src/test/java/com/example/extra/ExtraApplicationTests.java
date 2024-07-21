b41a3b0d805df2eb4340ef45b23d9427ca72c831
pangseon <chaosen0521@gmail.com>
2024-07-21 17:32:01 +0900

    first commit

---
+++ src/test/java/com/example/extra/ExtraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
