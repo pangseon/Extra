ae80ded96455783d01fd8745763f2007bfbc6368
hyeneung <imasaru22@gmail.com>
2024-07-23 03:41:05 +0900

    feature: 의상 컨펌 게시판 entity 작성

---
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/entity/CostumeApprovalBoard.java
@@ -0,0 +1,54 @@
+package com.example.extra.domain.costumeapprovalboard.entity;
+
+import com.example.extra.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "TB_COSTUME_APPROVAL_BOARD")
+@Entity
+public class CostumeApprovalBoard extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "boolean default false")
+    private Boolean costumeApprove;
+
+    @NotNull
+    private String costumeImageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Builder
+    public CostumeApprovalBoard(
+        Boolean costumeApprove,
+        String costumeImageUrl,
+        Member member,
+        Role role
+    ){
+        this.costumeApprove = costumeApprove;
+        this.costumeImageUrl = costumeImageUrl;
+        this.member = member;
+        this.role = role;
+    }
+}
