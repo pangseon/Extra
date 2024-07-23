4540bb70fa6df9c0be2df909ce841ed88ccec884
pangseon <chaosen0521@gmail.com>
2024-07-23 14:44:30 +0900

    feature: Category Entity 생성

---
+++ src/main/java/com/example/extra/global/enums/Category.java
@@ -0,0 +1,24 @@
+package com.example.extra.global.enums;
+
+public enum Category {
+    ROMANCE,
+    COMEDY,
+    DRAMA,
+    THRILLER,
+    HORROR,
+    MYSTERY,
+    FANTASY,
+    SF,
+    HISTORICAL,
+    ACTION,
+    ADVENTURE,
+    CRIME,
+    WAR,
+    FAMILY,
+    MELODRAMA,
+    MUSICAL,
+    SPORTS,
+    LEGAL,
+    MEDICAL,
+    POLITICAL
+}
