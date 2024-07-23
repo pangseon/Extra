71db2572bfbc01bb9b3f17e0f87c06a10b951c13
hyeneung <imasaru22@gmail.com>
2024-07-23 14:48:08 +0900

    feat: DB indexing 적용
    정렬 기준으로 자주 이용될 것들 위주로 인덱스 추가

--- src/main/java/com/example/extra/domain/applicationrequest/entity/ApplicationRequestCompany.java
+++ src/main/java/com/example/extra/domain/applicationrequest/entity/ApplicationRequestCompany.java
@@ -2,11 +2,13 @@ package com.example.extra.domain.applicationrequest.entity;
 
 import com.example.extra.global.common.BaseEntity;
 import com.example.extra.global.enums.ApplyStatus;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -17,13 +19,18 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "TB_APPLICATION_REQUEST_COMPANY")
+@Table(name = "TB_APPLICATION_REQUEST_COMPANY",
+    indexes = {
+        @Index(columnList = "createdAt")
+    }
+)
 @Entity
 public class ApplicationRequestCompany extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "apply_status", nullable = false)
     private ApplyStatus applyStatus;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
