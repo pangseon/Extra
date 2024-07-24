ddf0a3d197676d5e1de4d07572b54ee20a6736b9
pangseon <chaosen0521@gmail.com>
2024-07-24 13:37:15 +0900

    feature: CompanyRepository 생성 및 Company를 찾기 위한 메서드 추가

---
+++ src/main/java/com/example/extra/domain/company/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.example.extra.domain.company.repository;
+
+import com.example.extra.domain.company.entity.Company;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+    Optional<Company> findById(Long id);
+}
