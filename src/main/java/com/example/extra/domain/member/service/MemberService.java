c4693297030c5af39a9ae2a7f3a6c4467fedfb2a
shin0112 <wnslcosltimo12@gmail.com>
2024-07-25 12:09:15 +0900

    회원가입 서비스 로직 생성

--- src/main/java/com/example/extra/domain/member/service/MemberService.java
+++ src/main/java/com/example/extra/domain/member/service/MemberService.java
@@ -1,5 +1,13 @@
 package com.example.extra.domain.member.service;
 
+import com.example.extra.domain.member.dto.service.request.MemberCreateServiceRequestDto;
+import com.example.extra.domain.member.dto.service.response.MemberCreateServiceResponseDto;
+import com.example.extra.domain.tattoo.dto.service.request.TattooCreateServiceRequestDto;
+
 public interface MemberService {
 
+    MemberCreateServiceResponseDto signup(
+        final MemberCreateServiceRequestDto memberCreateServiceRequestDto,
+        final TattooCreateServiceRequestDto tattooCreateServiceRequestDto
+    );
 }
--- src/main/java/com/example/extra/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/example/extra/domain/member/service/impl/MemberServiceImpl.java
@@ -1,7 +1,14 @@
 package com.example.extra.domain.member.service.impl;
 
+import com.example.extra.domain.member.dto.service.request.MemberCreateServiceRequestDto;
+import com.example.extra.domain.member.dto.service.response.MemberCreateServiceResponseDto;
+import com.example.extra.domain.member.entity.Member;
+import com.example.extra.domain.member.mapper.entity.MemberEntityMapper;
 import com.example.extra.domain.member.repository.MemberRepository;
 import com.example.extra.domain.member.service.MemberService;
+import com.example.extra.domain.tattoo.dto.service.request.TattooCreateServiceRequestDto;
+import com.example.extra.domain.tattoo.entity.Tattoo;
+import com.example.extra.domain.tattoo.mapper.entity.TattooEntityMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +17,19 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final MemberEntityMapper memberEntityMapper;
+    private final TattooEntityMapper tattooEntityMapper;
 
+    @Override
+    public MemberCreateServiceResponseDto signup(
+        final MemberCreateServiceRequestDto memberCreateServiceRequestDto,
+        final TattooCreateServiceRequestDto tattooCreateServiceRequestDto
+    ) {
+        Tattoo tattoo = tattooEntityMapper.toTattoo(memberCreateServiceRequestDto);
+        Member member = memberEntityMapper.toMember(memberCreateServiceRequestDto, tattoo);
+
+        Member signupMember = memberRepository.save(member);
+
+        return memberEntityMapper.toMemberCreateServiceResponseDto(signupMember);
+    }
 }
