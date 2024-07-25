a1346542cb3c60c129fd8e63d55a0069409bb731
hyeneung <imasaru22@gmail.com>
2024-07-26 03:23:36 +0900

    refactor: ApplicationRequestCompany와 ApplicationRequestMember의 공통 부분을 ApplicationRequest 추상 클래스로 묶음

---
+++ src/main/java/com/example/extra/domain/applicationrequest/entity/ApplicationRequest.java
@@ -0,0 +1,37 @@
+package com.example.extra.domain.applicationrequest.entity;
+
+import com.example.extra.domain.member.entity.Member;
+import com.example.extra.domain.role.entity.Role;
+import com.example.extra.global.entity.BaseEntity;
+import com.example.extra.global.enums.ApplyStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+public abstract class ApplicationRequest extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "apply_status", nullable = false)
+    protected ApplyStatus applyStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id")
+    protected Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "role_id")
+    protected Role role;
+}
