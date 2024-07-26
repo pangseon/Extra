9c65c726cbabf12141111e6e5fe7928ebd386c3b
shin0112 <wnslcosltimo12@gmail.com>
2024-07-27 01:45:11 +0900

    feature: jwt 생성, 인증, 인가 필터 추가

---
+++ src/main/java/com/example/extra/global/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package com.example.extra.global.filter;
+
+import com.example.extra.domain.member.dto.controller.MemberLoginControllerRequestDto;
+import com.example.extra.domain.member.entity.Member;
+import com.example.extra.global.enums.UserRole;
+import com.example.extra.global.security.JwtUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Slf4j(topic = "로그인 & JWT 생성")
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Authentication attemptAuthentication(
+        HttpServletRequest request,
+        HttpServletResponse response
+    ) throws AuthenticationException {
+        log.info("로그인 시도");
+        try {
+            MemberLoginControllerRequestDto requestDto = new ObjectMapper()
+                .readValue(
+                    request.getInputStream(),
+                    MemberLoginControllerRequestDto.class
+                );
+
+            return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    requestDto.email(),
+                    requestDto.password()
+                )
+            );
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain chain,
+        Authentication authResult
+    ) throws IOException, ServletException {
+        log.info("로그인 성공 & jwt 생성 완료");
+        String username = ((Member) authResult.getPrincipal()).getUsername();
+        UserRole role = ((Member) authResult.getPrincipal()).getUserRole();
+
+        String token = jwtUtil.createToken(username, role);
+        jwtUtil.addJwtCookie(token, response);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException failed
+    ) throws IOException, ServletException {
+        log.info("로그인 실패");
+        response.setStatus(401);
+    }
+}
+
---
+++ src/main/java/com/example/extra/global/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,79 @@
+package com.example.extra.global.filter;
+
+import com.example.extra.global.security.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j(topic = "JWT 인증 & 인가")
+@Component
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+        @NotNull HttpServletRequest request,
+        @NotNull HttpServletResponse response,
+        @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (request.getRequestURI().equals("/api/v1/members/signup")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = jwtUtil.getJwtTokenFromRequest(request);
+        if (StringUtils.hasText(token)) {
+
+            token = jwtUtil.substringToken(token);
+            log.info(token);
+            if (!jwtUtil.validateToken(token)) {
+                log.error("Token Error");
+                return;
+            }
+            Claims info = jwtUtil.getUserInfoFromToken(token);
+            try {
+                setAuthentication(info.getSubject());
+            } catch (Exception e) {
+                log.error(e.getMessage());
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    public void setAuthentication(String username) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = createAuthentication(username);
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+    }
+
+    private Authentication createAuthentication(String username) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        return new UsernamePasswordAuthenticationToken(
+            userDetails,
+            null,
+            userDetails.getAuthorities()
+        );
+    }
+
+
+}
