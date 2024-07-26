f97b82c86f1c21f4e1a3c21c48808be0f0e5b095
hyeneung <imasaru22@gmail.com>
2024-07-27 04:52:20 +0900

    style: 불필요한 exception들 제거
    기존에는 front측에서 업데이트 되지 않은 정보를 바탕으로 잘못된 요청을 내릴 경우를 상정하였으나, 사용자가 이미 지원한 역할에 대해 다시 지원할 일은 없다고 판단하여 제거함

--- src/main/java/com/example/extra/domain/applicationrequest/exception/ApplicationRequestErrorCode.java
+++ src/main/java/com/example/extra/domain/applicationrequest/exception/ApplicationRequestErrorCode.java
@@ -9,15 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ApplicationRequestErrorCode implements ErrorCode {
     // 400
-    ALREADY_EXIST_APPLICATION_REQUEST_COMPANY(HttpStatus.BAD_REQUEST, "이미 해당 사용자에게 요청되었습니다."),
-    ALREADY_EXIST_APPLICATION_REQUEST_MEMBER(HttpStatus.BAD_REQUEST, "이미 해당 역할에 지원되었습니다."),
-
     NOT_ABLE_TO_CANCEL_APPLICATION_REQUEST_MEMBER(HttpStatus.BAD_REQUEST, "승인 상태에서는 지원 철회가 불가합니다."),
-
-    // 404
-    NOT_FOUND_APPLICATION_REQUEST_COMPANY(HttpStatus.NOT_FOUND, "해당 사용자에게 요청한 이력이 없습니다."),
-    NOT_FOUND_APPLICATION_REQUEST_MEMBER(HttpStatus.NOT_FOUND, "해당 역할에 지원한 이력이 없습니다.");
-
+    NOT_FOUND_APPLICATION_REQUEST_MEMBER(HttpStatus.NOT_FOUND, "해당 역할은 더이상 유효하지 않습니다.");
     private final HttpStatus status;
     private final String message;
 }
--- src/main/java/com/example/extra/domain/applicationrequest/exception/NotFoundApplicationRequestMemberException.java
+++ src/main/java/com/example/extra/domain/applicationrequest/exception/NotFoundApplicationRequestMemberException.java
@@ -4,7 +4,8 @@ package com.example.extra.domain.applicationrequest.exception;
 import com.example.extra.global.exception.CustomException;
 
 public class NotFoundApplicationRequestMemberException extends CustomException {
-    // 삭제하려 했는데 없을 때
+    // 이미 회사 측에서 삭제한 역할에 대해서 지원 취소를 하려 할 때
+    // front 단에서는 지원한 상태로 보여지는데 실제로는 DB에서 삭제된 경우 있을 수 있어서 서버에서 검증
     public NotFoundApplicationRequestMemberException(final ApplicationRequestErrorCode errorCode) {
         super(errorCode);
     }
