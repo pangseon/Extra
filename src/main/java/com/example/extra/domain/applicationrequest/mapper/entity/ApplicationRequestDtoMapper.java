849d7c9a72de6865b743116eaeeac187666355c5
hyeneung <imasaru22@gmail.com>
2024-07-27 06:59:35 +0900

    feature: 역할과 연관된 출연자들 확인 관련 dto mapper 작성

--- src/main/java/com/example/extra/domain/applicationrequest/mapper/entity/ApplicationRequestDtoMapper.java
+++ src/main/java/com/example/extra/domain/applicationrequest/mapper/entity/ApplicationRequestDtoMapper.java
@@ -1,6 +1,8 @@
 package com.example.extra.domain.applicationrequest.mapper.entity;
 
+import com.example.extra.domain.applicationrequest.dto.service.ApplicationRequestCompanyReadServiceResponseDto;
 import com.example.extra.domain.applicationrequest.dto.service.ApplicationRequestMemberReadServiceResponseDto;
+import com.example.extra.domain.member.entity.Member;
 import com.example.extra.domain.role.entity.Role;
 import java.util.List;
 import org.mapstruct.Mapper;
@@ -13,6 +15,9 @@ public interface ApplicationRequestDtoMapper {
     @Mapping(source = "id", target = "id")
     ApplicationRequestMemberReadServiceResponseDto toApplicationRequestMemberReadServiceResponseDto(Role role);
 
-    List<ApplicationRequestMemberReadServiceResponseDto> toApplicationRequestMemberReadServiceResponseDtoList(List<Role> role);
+    List<ApplicationRequestMemberReadServiceResponseDto> toApplicationRequestMemberReadServiceResponseDtoList(List<Role> roleList);
 
+    @Mapping(source = "id", target = "id")
+    ApplicationRequestCompanyReadServiceResponseDto toApplicationRequestCompanyReadServiceResponseDto(Member member);
+    List<ApplicationRequestCompanyReadServiceResponseDto> toApplicationRequestCompanyReadServiceResponseDtoList(List<Member> member);
 }
