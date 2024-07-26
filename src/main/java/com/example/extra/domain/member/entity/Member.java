832b2548de159e91fef47e5d15d47449e316bdf8
shin0112 <wnslcosltimo12@gmail.com>
2024-07-26 15:56:04 +0900

    feature: one to many 필드 주석화

--- src/main/java/com/example/extra/domain/member/entity/Member.java
+++ src/main/java/com/example/extra/domain/member/entity/Member.java
@@ -1,25 +1,17 @@
 package com.example.extra.domain.member.entity;
 
-import com.example.extra.domain.applicationrequest.entity.ApplicationRequestCompany;
-import com.example.extra.domain.applicationrequest.entity.ApplicationRequestMember;
-import com.example.extra.domain.attendancemanagement.entity.AttendanceManagement;
-import com.example.extra.domain.costumeapprovalboard.entity.CostumeApprovalBoard;
-import com.example.extra.domain.memberterms.entity.MemberTerms;
 import com.example.extra.domain.tattoo.entity.Tattoo;
 import com.example.extra.global.entity.BaseEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -84,20 +76,20 @@ public class Member extends BaseEntity {
     @ToString.Exclude
     private Tattoo tattoo;
 
-    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private final List<ApplicationRequestCompany> applicationRequestCompanyList = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private final List<ApplicationRequestMember> applicationRequestMemberList = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private final List<AttendanceManagement> attendanceManagementList = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private final List<CostumeApprovalBoard> costumeApprovalBoardList = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private final List<MemberTerms> memberTermsList = new ArrayList<>();
+//    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+//    private final List<ApplicationRequestCompany> applicationRequestCompanyList = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+//    private final List<ApplicationRequestMember> applicationRequestMemberList = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+//    private final List<AttendanceManagement> attendanceManagementList = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+//    private final List<CostumeApprovalBoard> costumeApprovalBoardList = new ArrayList<>();
+//
+//    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+//    private final List<MemberTerms> memberTermsList = new ArrayList<>();
 
     @Builder
     public Member(
