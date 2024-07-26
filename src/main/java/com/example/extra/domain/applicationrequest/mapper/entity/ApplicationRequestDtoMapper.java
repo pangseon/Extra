ad2797b6b3de53ce149b3ab7f48d74536e56dd84
hyeneung <imasaru22@gmail.com>
2024-07-27 04:59:01 +0900

    bug: mapper의 source target 명시
    test 과정에서 mapper의 문제로 null만 받았기에 source target을 명시하여 제대로 매핑되도록 함.

--- src/main/java/com/example/extra/domain/applicationrequest/mapper/entity/ApplicationRequestDtoMapper.java
+++ src/main/java/com/example/extra/domain/applicationrequest/mapper/entity/ApplicationRequestDtoMapper.java
@@ -4,11 +4,13 @@ import com.example.extra.domain.applicationrequest.dto.service.ApplicationReques
 import com.example.extra.domain.role.entity.Role;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = ComponentModel.SPRING, unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface ApplicationRequestDtoMapper {
+    @Mapping(source = "id", target = "id")
     ApplicationRequestMemberReadServiceResponseDto toApplicationRequestMemberReadServiceResponseDto(Role role);
 
     List<ApplicationRequestMemberReadServiceResponseDto> toApplicationRequestMemberReadServiceResponseDtoList(List<Role> role);
