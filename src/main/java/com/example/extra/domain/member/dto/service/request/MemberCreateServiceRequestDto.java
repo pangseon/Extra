a96f58ba6a621b81cfcf13084be488b4fdd1c8bd
shin0112 <wnslcosltimo12@gmail.com>
2024-07-26 22:51:15 +0900

    sytle : fix typos

--- src/main/java/com/example/extra/domain/member/dto/service/request/MemberCreateServiceRequestDto.java
+++ src/main/java/com/example/extra/domain/member/dto/service/request/MemberCreateServiceRequestDto.java
@@ -13,13 +13,7 @@ public record MemberCreateServiceRequestDto(
     Float height,
     Float weight,
     String bank,
-    String account_number
-
-    /**
-     * 계좌 관련 정보(임시)
-     * bank : 은행명
-     * account_number : 계좌번호
-     */
+    String accountNumber,
 
     /**
      * 약관 동의 내용 필요
