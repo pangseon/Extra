d04135ded7a8fcc8e2f5ef69aafc1bd0743ef4ff
hyeneung <imasaru22@gmail.com>
2024-07-27 02:56:42 +0900

    style: API 수정 하여 path variable 이용

--- src/main/java/com/example/extra/domain/applicationrequest/controller/ApplicationRequestMemberController.java
+++ src/main/java/com/example/extra/domain/applicationrequest/controller/ApplicationRequestMemberController.java
@@ -1,84 +1,67 @@
 package com.example.extra.domain.applicationrequest.controller;
 
-import com.example.extra.domain.applicationrequest.dto.controller.ApplicationRequestCreateControllerRequestDto;
-import com.example.extra.domain.applicationrequest.dto.controller.ApplicationRequestDeleteControllerRequestDto;
-import com.example.extra.domain.applicationrequest.dto.service.ApplicationRequestCreateServiceRequestDto;
-import com.example.extra.domain.applicationrequest.dto.service.ApplicationRequestDeleteServiceRequestDto;
-import com.example.extra.domain.applicationrequest.mapper.dto.ApplicationRequestDtoMapper;
+import com.example.extra.domain.applicationrequest.dto.service.ApplicationRequestMemberReadServiceResponseDto;
 import com.example.extra.domain.applicationrequest.service.ApplicationRequestMemberService;
-import com.example.extra.domain.role.dto.controller.RoleCreateControllerRequestDto;
 import com.example.extra.global.enums.ApplyStatus;
-import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/applicationRequest/member")
+@RequestMapping("/api/v1/application-request/member")
 @RestController
 public class ApplicationRequestMemberController {
-    private final ApplicationRequestDtoMapper applicationRequestDtoMapper;
     private final ApplicationRequestMemberService applicationRequestMemberService;
 
-    @PostMapping("/application")
+    @PostMapping("/role/{roleId}")
     public ResponseEntity<?> createApplicationRequestMember(
-        @Valid @RequestPart(name = "applicationRequestCreateControllerRequestDto")
-        ApplicationRequestCreateControllerRequestDto applicationRequestCreateControllerRequestDto
+        @PathVariable(name = "roleId") Long roleId
     ){
-        ApplicationRequestCreateServiceRequestDto applicationRequestCreateServiceRequestDto =
-            applicationRequestDtoMapper.toApplicationRequestCreateServiceRequestDto(applicationRequestCreateControllerRequestDto);
-
-        applicationRequestMemberService.createApplicationRequestMember(applicationRequestCreateServiceRequestDto);
+        applicationRequestMemberService.createApplicationRequestMember(roleId);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("/application")
+    @GetMapping("/roles")
     public ResponseEntity<?> readAllApplicationRequestMember(
         @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable
     ){
-        // TODO - RoleReadResponseDto 정해지면 바꾸기
-        Slice<RoleCreateControllerRequestDto> appliedRoleSlice =
+        List<ApplicationRequestMemberReadServiceResponseDto> appliedRoleList =
             applicationRequestMemberService.getAppliedRoles(pageable);
 
         return ResponseEntity.status(HttpStatus.OK)
-            .body(appliedRoleSlice);
+            .body(appliedRoleList);
     }
 
-    @GetMapping("/application/{status}")
+    @GetMapping("/roles/{status}")
     public ResponseEntity<?> readAllApplicationRequestMemberByStatus(
         @PathVariable(name = "status") ApplyStatus applyStatus,
         @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable
     ){
-        // TODO - RoleReadResponseDto 정해지면 바꾸기
-        Slice<RoleCreateControllerRequestDto> appliedRoleSlice =
+        List<ApplicationRequestMemberReadServiceResponseDto> appliedRoleList =
             applicationRequestMemberService.getAppliedRolesByStatus(
                 applyStatus,
                 pageable
         );
         return ResponseEntity.status(HttpStatus.OK)
-            .body(appliedRoleSlice);
+            .body(appliedRoleList);
     }
 
-    @PostMapping("application/delete")
+    @DeleteMapping("role/{roleId}")
     public ResponseEntity<?> deleteApplicationRequestMember(
-        @Valid @RequestPart(name = "applicationRequestDeleteControllerRequestDto")
-        ApplicationRequestDeleteControllerRequestDto applicationRequestDeleteControllerRequestDto
+        @PathVariable(name = "roleId") Long roleId
     ){
-        ApplicationRequestDeleteServiceRequestDto applicationRequestDeleteServiceRequestDto =
-            applicationRequestDtoMapper.toApplicationRequestDeleteServiceRequestDto(applicationRequestDeleteControllerRequestDto);
-
-        applicationRequestMemberService.deleteApplicationRequestMember(applicationRequestDeleteServiceRequestDto);
+        applicationRequestMemberService.deleteApplicationRequestMember(roleId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
