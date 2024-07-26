db6e5f60d360530ced9e5fccce905cd386cee071
hyeneung <imasaru22@gmail.com>
2024-07-27 08:37:16 +0900

    refactor: 엔티티 만들 필요 없는 것들 id 기반 쿼리로 대체

--- src/main/java/com/example/extra/domain/applicationrequest/repository/ApplicationRequestMemberRepository.java
+++ src/main/java/com/example/extra/domain/applicationrequest/repository/ApplicationRequestMemberRepository.java
@@ -1,8 +1,6 @@
 package com.example.extra.domain.applicationrequest.repository;
 
 import com.example.extra.domain.applicationrequest.entity.ApplicationRequestMember;
-import com.example.extra.domain.member.entity.Member;
-import com.example.extra.domain.role.entity.Role;
 import com.example.extra.global.enums.ApplyStatus;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
@@ -10,19 +8,24 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicationRequestMemberRepository extends JpaRepository<ApplicationRequestMember, Long> {
-    Optional<ApplicationRequestMember> findByMemberAndRole(Member member, Role role);
-    Slice<ApplicationRequestMember> findAllByMember(
-        Member member,
+    Optional<ApplicationRequestMember> findByMemberIdAndRoleId(Long memberId, Long roleId);
+    Slice<ApplicationRequestMember> findAllByMemberId(
+        Long memberId,
         Pageable page
     );
-    Slice<ApplicationRequestMember> findAllByMemberAndApplyStatus(
-        Member member,
+    Slice<ApplicationRequestMember> findAllByMemberIdAndApplyStatus(
+        Long memberId,
         ApplyStatus applyStatus,
         Pageable page
     );
 
     Slice<ApplicationRequestMember> findAllByRoleId(
         Long roleId,
+        Pageable page
+    );
+    Slice<ApplicationRequestMember> findAllByRoleIdAndApplyStatus(
+        Long roleId,
+        ApplyStatus applyStatus,
         Pageable page
     );
 
