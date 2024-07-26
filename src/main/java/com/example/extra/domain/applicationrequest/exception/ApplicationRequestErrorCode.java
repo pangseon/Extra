a541a5ac7691fa771de78889f77410a3a2bd06df
hyeneung <imasaru22@gmail.com>
2024-07-27 07:04:28 +0900

    feature: 업체 측에서 지원 승인, 거절하는 기능 관련 exception 작성

--- src/main/java/com/example/extra/domain/applicationrequest/exception/ApplicationRequestErrorCode.java
+++ src/main/java/com/example/extra/domain/applicationrequest/exception/ApplicationRequestErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum ApplicationRequestErrorCode implements ErrorCode {
     // 400
     NOT_ABLE_TO_CANCEL_APPLICATION_REQUEST_MEMBER(HttpStatus.BAD_REQUEST, "승인 상태에서는 지원 철회가 불가합니다."),
-    NOT_FOUND_APPLICATION_REQUEST_MEMBER(HttpStatus.NOT_FOUND, "해당 역할은 더이상 유효하지 않습니다.");
+    NOT_FOUND_APPLICATION_REQUEST_MEMBER(HttpStatus.NOT_FOUND, "해당 역할은 더이상 유효하지 않습니다."),
+    NOT_FOUND_APPLICATION_REQUEST_COMPANY(HttpStatus.NOT_FOUND, "해당 지원자는 더이상 유효하지 않습니다.");
+
     private final HttpStatus status;
     private final String message;
 }
