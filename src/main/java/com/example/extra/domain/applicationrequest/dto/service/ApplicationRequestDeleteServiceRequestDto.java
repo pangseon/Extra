3bb4370e0f6bd4d674c15ff3e7fac0dc9fdf82f9
hyeneung <imasaru22@gmail.com>
2024-07-27 02:50:10 +0900

    style: roleId를 request body로 받기 위해 작성한 dto들 삭제

--- src/main/java/com/example/extra/domain/applicationrequest/dto/service/ApplicationRequestDeleteServiceRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.example.extra.domain.applicationrequest.dto.service;
-
-// controller 단에서 mapper에 의해 request body가 얘로 바뀌어 서비스 매개변수로
-public record ApplicationRequestDeleteServiceRequestDto(
-    Long roleId
-) {
-
-}
