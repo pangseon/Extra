561e48ba5ec5787bebcd9ed5423214aa0321f72d
pangseon <chaosen0521@gmail.com>
2024-07-27 16:00:36 +0900

    feature: JobPost Update를 위한 로직 작성

--- src/main/java/com/example/extra/domain/jobpost/service/JobPostService.java
+++ src/main/java/com/example/extra/domain/jobpost/service/JobPostService.java
@@ -1,12 +1,23 @@
 package com.example.extra.domain.jobpost.service;
 
 import com.example.extra.domain.jobpost.dto.service.JobPostCreateServiceRequestDto;
+import com.example.extra.domain.jobpost.dto.service.JobPostUpdateServiceRequestDto;
 import com.example.extra.domain.role.dto.service.RoleCreateServiceRequestDto;
 import java.util.List;
 
 public interface JobPostService {
     void createJobPost(
         final JobPostCreateServiceRequestDto jobPostCreateServiceRequestDto,
-        final List<RoleCreateServiceRequestDto> roleCreateServiceRequestDtoList);
+        final List<RoleCreateServiceRequestDto> roleCreateServiceRequestDtoList
+    );
+    void updateJobPost(
+        Long jobPost_id,
+        final JobPostUpdateServiceRequestDto jobPostUpdateServiceRequestDto
+    );
+/*
+    void deleteJobPost(
+        Long jobPost_id
+    );
+*/
 
 }
