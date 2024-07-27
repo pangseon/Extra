5d892b48585b5d51154f116697bbb6208639b029
shin0112 <wnslcosltimo12@gmail.com>
2024-07-28 03:42:13 +0900

    feature: role 변경

--- src/main/java/com/example/extra/global/enums/UserRole.java
+++ src/main/java/com/example/extra/global/enums/UserRole.java
@@ -1,12 +1,22 @@
 package com.example.extra.global.enums;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum UserRole {
-    ROLE_USER("ROLE_USER"), ROLE_ADMIN("ROLE_ADMIN");
+    USER(Authority.USER),
+    ADMIN(Authority.ADMIN);
 
-    private final String role;
+    private final String authority;
+
+    UserRole(String authority) {
+        this.authority = authority;
+    }
+
+    public static class Authority {
+
+        public static final String USER = "ROLE_USER";
+        public static final String ADMIN = "ROLE_ADMIN";
+    }
+
 }
