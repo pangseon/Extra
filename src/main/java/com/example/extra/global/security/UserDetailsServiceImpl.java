b4914771def3c096d6908860d92474d6f9a27032
shin0112 <wnslcosltimo12@gmail.com>
2024-07-28 02:27:11 +0900

    feature: 로그 추가

--- src/main/java/com/example/extra/global/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/extra/global/security/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.extra.domain.member.exception.MemberErrorCode;
 import com.example.extra.domain.member.exception.MemberException;
 import com.example.extra.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("사용자 아이디 " + username);
         return memberRepository.findByEmail(username)
             .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
     }
