e87b6623bb999d184cd7345990dbfbaf8b2ef19e
shin0112 <wnslcosltimo12@gmail.com>
2024-08-01 19:37:22 +0900

    feature: 토큰 재발급 api 파라미터 변경

--- src/main/java/com/example/extra/global/security/controller/RefreshTokenController.java
+++ src/main/java/com/example/extra/global/security/controller/RefreshTokenController.java
@@ -1,11 +1,11 @@
 package com.example.extra.global.security.controller;
 
+import com.example.extra.global.security.UserDetailsImpl;
 import com.example.extra.global.security.service.RefreshTokenService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +20,12 @@ public class RefreshTokenController {
 
     @GetMapping("/token")
     public ResponseEntity<?> getRefreshToken(
-        @AuthenticationPrincipal Authentication authentication,
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
         HttpServletRequest httpServletRequest,
         HttpServletResponse httpServletResponse
     ) {
         refreshTokenService.getNewAccessToken(
-            authentication,
+            userDetails,
             httpServletRequest,
             httpServletResponse
         );
