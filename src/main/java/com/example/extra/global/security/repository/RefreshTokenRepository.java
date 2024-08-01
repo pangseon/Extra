1208c86e6326a26d978da4fec14fa1770808e9d7
shin0112 <wnslcosltimo12@gmail.com>
2024-08-01 19:33:22 +0900

    refactor: access token field 삭제

--- src/main/java/com/example/extra/global/security/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/extra/global/security/repository/RefreshTokenRepository.java
@@ -1,12 +1,10 @@
 package com.example.extra.global.security.repository;
 
 import com.example.extra.global.security.token.RefreshToken;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
 
-    Optional<RefreshToken> findByAccessToken(String accessToken);
 }
--- src/main/java/com/example/extra/global/security/token/RefreshToken.java
+++ src/main/java/com/example/extra/global/security/token/RefreshToken.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
 @AllArgsConstructor
@@ -21,7 +20,5 @@ public class RefreshToken {
     @Id
     private Long id;
     private String refreshToken;
-    @Indexed
-    private String accessToken;
 
 }
