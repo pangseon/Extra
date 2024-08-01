546cd1df52cd389d21df26927547ec48ddb55bcd
shin0112 <wnslcosltimo12@gmail.com>
2024-08-01 19:24:58 +0900

    style: mapping 형태 변경

--- src/main/java/com/example/extra/domain/member/controller/MemberController.java
+++ src/main/java/com/example/extra/domain/member/controller/MemberController.java
@@ -24,7 +24,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -77,7 +76,7 @@ public class MemberController {
             .build();
     }
 
-    @PutMapping("/logout")
+    @PostMapping("/logout")
     public ResponseEntity<?> logout(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @NotNull HttpServletRequest request
