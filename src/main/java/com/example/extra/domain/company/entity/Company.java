d34f822b86802f77d58d229349e109d2e32695b9
shin0112 <wnslcosltimo12@gmail.com>
2024-08-02 19:40:05 +0900

    feature: 회원 가입 용 role 추가

--- src/main/java/com/example/extra/domain/company/entity/Company.java
+++ src/main/java/com/example/extra/domain/company/entity/Company.java
@@ -2,6 +2,7 @@ package com.example.extra.domain.company.entity;
 
 import com.example.extra.domain.jobpost.entity.JobPost;
 import com.example.extra.global.entity.BaseEntity;
+import com.example.extra.global.enums.UserRole;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -23,6 +24,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "TB_COMPANY")
 @Entity
 public class Company extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -39,6 +41,8 @@ public class Company extends BaseEntity {
     @Column(name = "company_url")
     private String companyUrl;
 
+    private UserRole userRole;
+
     // TODO - 회사-공고글 양방향 매핑할 지 확인 받기 + cascade 정책 확인 받기
     @OneToMany(mappedBy = "company", cascade = CascadeType.ALL)
     private List<JobPost> jobPostList = new ArrayList<>();
@@ -52,10 +56,16 @@ public class Company extends BaseEntity {
         String password,
         String name,
         String companyUrl
-    ){
+    ) {
         this.email = email;
         this.password = password;
         this.name = name;
         this.companyUrl = companyUrl;
+        this.userRole = UserRole.USER;
     }
+
+    public void updateRole() {
+        this.userRole = UserRole.ADMIN;
+    }
+
 }
