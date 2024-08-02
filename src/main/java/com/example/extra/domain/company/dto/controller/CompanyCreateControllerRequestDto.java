67b138a7a7101b02bf29f1f6e7f70bfdd9410b96
shin0112 <wnslcosltimo12@gmail.com>
2024-08-02 21:34:55 +0900

    feature: dto에 유효성 검사 추가

--- src/main/java/com/example/extra/domain/company/dto/controller/CompanyCreateControllerRequestDto.java
+++ src/main/java/com/example/extra/domain/company/dto/controller/CompanyCreateControllerRequestDto.java
@@ -1,8 +1,13 @@
 package com.example.extra.domain.company.dto.controller;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CompanyCreateControllerRequestDto(
+    @NotNull
     String email,
+    @NotNull
     String password,
+    @NotNull
     String name,
     boolean idAdmin,
     String adminToken
