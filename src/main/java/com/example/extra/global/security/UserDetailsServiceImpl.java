37b50b1429f45bf33fdb35ec164b357280a38616
shin0112 <wnslcosltimo12@gmail.com>
2024-08-02 19:40:33 +0900

    feature: 회사/회원 확인을 위한 조건문 작성

--- src/main/java/com/example/extra/global/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/extra/global/security/UserDetailsServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.extra.global.security;
 
+import com.example.extra.domain.company.entity.Company;
+import com.example.extra.domain.company.exception.CompanyErrorCode;
+import com.example.extra.domain.company.exception.CompanyException;
+import com.example.extra.domain.company.repository.CompanyRepository;
 import com.example.extra.domain.member.entity.Member;
-import com.example.extra.domain.member.exception.MemberErrorCode;
-import com.example.extra.domain.member.exception.MemberException;
 import com.example.extra.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,12 +19,23 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final MemberRepository memberRepository;
+    private final CompanyRepository companyRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("사용자 아이디 " + username);
+
+        // member 확인
         Member member = memberRepository.findByEmail(username)
-            .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
+            .orElse(null);
+
+        // company 가입 일 경우
+        if (member == null) {
+            Company company = companyRepository.findByEmail(username)
+                .orElseThrow(() -> new CompanyException(CompanyErrorCode.NOT_FOUND_COMPANY));
+            return new UserDetailsImpl(company);
+        }
+
         return new UserDetailsImpl(member);
     }
 }
