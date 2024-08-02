5002537d078fe557aab73bb28f592fbb2cd3262e
shin0112 <wnslcosltimo12@gmail.com>
2024-08-02 21:38:14 +0900

    feature: service 로그인 작성

--- src/main/java/com/example/extra/domain/company/service/CompanyService.java
+++ src/main/java/com/example/extra/domain/company/service/CompanyService.java
@@ -1,10 +1,16 @@
 package com.example.extra.domain.company.service;
 
 import com.example.extra.domain.company.dto.service.request.CompanyCreateServiceRequestDto;
+import com.example.extra.domain.company.dto.service.request.CompanyLoginServiceRequestDto;
+import com.example.extra.domain.company.dto.service.response.CompanyLoginServiceResponseDto;
 
 public interface CompanyService {
 
     void signup(
         CompanyCreateServiceRequestDto companyCreateServiceRequestDto
+    );
+
+    CompanyLoginServiceResponseDto login(
+        CompanyLoginServiceRequestDto companyLoginServiceRequestDto
     );
 }
--- src/main/java/com/example/extra/domain/company/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/example/extra/domain/company/service/impl/CompanyServiceImpl.java
@@ -1,26 +1,35 @@
 package com.example.extra.domain.company.service.impl;
 
-import com.example.extra.domain.company.dto.service.CompanyCreateServiceRequestDto;
+import com.example.extra.domain.company.dto.service.request.CompanyCreateServiceRequestDto;
+import com.example.extra.domain.company.dto.service.request.CompanyLoginServiceRequestDto;
+import com.example.extra.domain.company.dto.service.response.CompanyLoginServiceResponseDto;
 import com.example.extra.domain.company.entity.Company;
 import com.example.extra.domain.company.exception.CompanyErrorCode;
 import com.example.extra.domain.company.exception.CompanyException;
 import com.example.extra.domain.company.mapper.entity.CompanyEntityMapper;
 import com.example.extra.domain.company.repository.CompanyRepository;
 import com.example.extra.domain.company.service.CompanyService;
+import com.example.extra.global.security.JwtUtil;
+import com.example.extra.global.security.repository.RefreshTokenRepository;
+import com.example.extra.global.security.token.RefreshToken;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CompanyServiceImpl implements CompanyService {
 
     private final CompanyRepository companyRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
     private final CompanyEntityMapper companyEntityMapper;
 
     // security
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     private final String ADMIN_TOKEN = "admintoken";
 
@@ -48,5 +57,39 @@ public class CompanyServiceImpl implements CompanyService {
             company.updateRole();
             companyRepository.save(company);
         }
+    }
+
+    @Override
+    public CompanyLoginServiceResponseDto login(
+        CompanyLoginServiceRequestDto companyLoginServiceRequestDto) {
+        Company company = companyRepository.findByEmail(companyLoginServiceRequestDto.email())
+            .orElseThrow(() -> new CompanyException(CompanyErrorCode.NOT_FOUND_COMPANY));
+
+        if (!passwordEncoder.matches(
+            companyLoginServiceRequestDto.password(),
+            company.getPassword()
+        )) {
+            throw new CompanyException(CompanyErrorCode.INVALID_PASSWORD);
+        }
+
+        // jwt 토큰 생성
+        String accessToken = jwtUtil.createToken(
+            company.getEmail(),
+            company.getUserRole()
+        );
+        String refreshToken = jwtUtil.createRefreshToken();
+        log.info("access token: " + accessToken);
+        log.info("refresh token: " + refreshToken);
+
+        company.updateRefreshToken(jwtUtil.substringToken(refreshToken));
+
+        refreshTokenRepository.save(
+            new RefreshToken(
+                company.getId(),
+                jwtUtil.substringToken(refreshToken)
+            )
+        );
+
+        return new CompanyLoginServiceResponseDto(accessToken);
     }
 }
