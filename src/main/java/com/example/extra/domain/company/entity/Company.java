c7f110fbb10f388a6cbb8b3393ea20cf58970b1c
shin0112 <wnslcosltimo12@gmail.com>
2024-08-02 21:16:12 +0900

    style: enum 타입 변경

--- src/main/java/com/example/extra/domain/company/entity/Company.java
+++ src/main/java/com/example/extra/domain/company/entity/Company.java
@@ -6,6 +6,8 @@ import com.example.extra.global.enums.UserRole;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -41,6 +43,7 @@ public class Company extends BaseEntity {
     @Column(name = "company_url")
     private String companyUrl;
 
+    @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
     // TODO - 회사-공고글 양방향 매핑할 지 확인 받기 + cascade 정책 확인 받기
