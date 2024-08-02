26cbca063f1772e1db103a25431152e7c8863ce8
hyeneung <imasaru22@gmail.com>
2024-08-02 20:41:46 +0900

    feature: 출연자 관리 관련 entity 작성

--- src/main/java/com/example/extra/domain/attendancemanagement/entity/AttendanceManagement.java
+++ src/main/java/com/example/extra/domain/attendancemanagement/entity/AttendanceManagement.java
@@ -11,13 +11,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -34,16 +34,11 @@ public class AttendanceManagement extends BaseEntity {
     @Column(name = "clock_out_time")
     private LocalDateTime clockOutTime;
 
-    @Column(columnDefinition = "boolean default false")
-    private Boolean breakfast;
+    @Column(name = "meal_count")
+    @ColumnDefault("0")
+    private Integer mealCount;
 
-    @Column(columnDefinition = "boolean default false")
-    private Boolean lunch;
-
-    @Column(columnDefinition = "boolean default false")
-    private Boolean dinner;
-
-    @OneToOne(optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "job_post_id")
     private JobPost jobPost;
 
@@ -55,18 +50,24 @@ public class AttendanceManagement extends BaseEntity {
     public AttendanceManagement(
         LocalDateTime clockInTime,
         LocalDateTime clockOutTime,
-        Boolean breakfast,
-        Boolean lunch,
-        Boolean dinner,
+        Integer mealCount,
         JobPost jobPost,
         Member member
     ){
         this.clockInTime = clockInTime;
         this.clockOutTime = clockOutTime;
-        this.breakfast = breakfast;
-        this.lunch = lunch;
-        this.dinner = dinner;
+        this.mealCount = mealCount;
         this.jobPost = jobPost;
         this.member = member;
+    }
+
+    public void updateClockInTimeTo(LocalDateTime clockInTime){
+        this.clockInTime = clockInTime;
+    }
+    public void updateClockOutTimeTo(LocalDateTime clockOutTime){
+        this.clockOutTime = clockOutTime;
+    }
+    public void addOneToMealCount(){
+        this.mealCount += 1;
     }
 }
