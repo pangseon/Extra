959c28c4a212808fb9133483ef95b9d87e7dfab2
shin0112 <wnslcosltimo12@gmail.com>
2024-08-02 19:53:00 +0900

    feature: 회사 회원 가입용 dto mapper 작성

---
+++ src/main/java/com/example/extra/domain/company/mapper/dto/CompanyDtoMapper.java
@@ -0,0 +1,16 @@
+package com.example.extra.domain.company.mapper.dto;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+import com.example.extra.domain.company.dto.controller.CompanyCreateControllerRequestDto;
+import com.example.extra.domain.company.dto.service.CompanyCreateServiceRequestDto;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = SPRING)
+public interface CompanyDtoMapper {
+
+    CompanyCreateServiceRequestDto toCompanyCreateServiceRequestDto(
+        CompanyCreateControllerRequestDto companyCreateControllerRequestDto
+    );
+
+}
