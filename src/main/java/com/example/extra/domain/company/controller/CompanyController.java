1fea87ca771ab3ee78ba35435e9e6ac0aa5d92cb
shin0112 <wnslcosltimo12@gmail.com>
2024-08-02 20:46:11 +0900

    feature: controller 회원 가입 작성

--- src/main/java/com/example/extra/domain/company/controller/CompanyController.java
+++ src/main/java/com/example/extra/domain/company/controller/CompanyController.java
@@ -1,8 +1,15 @@
 package com.example.extra.domain.company.controller;
 
+import com.example.extra.domain.company.dto.controller.CompanyCreateControllerRequestDto;
+import com.example.extra.domain.company.dto.service.CompanyCreateServiceRequestDto;
 import com.example.extra.domain.company.mapper.dto.CompanyDtoMapper;
 import com.example.extra.domain.company.service.CompanyService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +21,17 @@ public class CompanyController {
     private final CompanyService companyService;
     private final CompanyDtoMapper companyDtoMapper;
 
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(
+        @Valid @RequestBody CompanyCreateControllerRequestDto companyCreateControllerRequestDto
+    ) {
+        CompanyCreateServiceRequestDto companyCreateServiceRequestDto =
+            companyDtoMapper.toCompanyCreateServiceRequestDto(companyCreateControllerRequestDto);
 
+        companyService.signup(
+            companyCreateServiceRequestDto
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
