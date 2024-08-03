d6dbb815baed544659b52a79656546ce21c475a4
hyeneung <imasaru22@gmail.com>
2024-08-04 03:33:41 +0900

    feature: 단건 조회 repository 작성

--- src/main/java/com/example/extra/domain/costumeapprovalboard/repository/CostumeApprovalBoardRepository.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/repository/CostumeApprovalBoardRepository.java
@@ -1,8 +1,10 @@
 package com.example.extra.domain.costumeapprovalboard.repository;
 
 import com.example.extra.domain.costumeapprovalboard.entity.CostumeApprovalBoard;
+import com.example.extra.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CostumeApprovalBoardRepository extends JpaRepository<CostumeApprovalBoard, Long> {
-
+    Optional<CostumeApprovalBoard> findByMemberAndRoleId(Member member, Long roleId);
 }
