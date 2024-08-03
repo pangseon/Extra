482189c90244e7ec626cc1b109e00d5fe0fb73d5
pangseon <chaosen0521@gmail.com>
2024-08-03 19:39:46 +0900

    feature: 역할 데이터 업데이트를 위한 Dto 생성

---
+++ src/main/java/com/example/extra/domain/role/dto/controller/RoleUpdateControllerRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.extra.domain.role.dto.controller;
+
+import com.example.extra.global.enums.Season;
+import java.time.LocalDate;
+
+public record RoleUpdateControllerRequestDto(
+    String role_name,
+    String costume,
+    Boolean sex,
+    LocalDate role_age,
+    Integer limit_personnel,
+    Integer current_personnel,
+    Season season
+)
+{
+
+}
---
+++ src/main/java/com/example/extra/domain/role/dto/service/RoleUpdateServiceRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.extra.domain.role.dto.service;
+
+import com.example.extra.global.enums.Season;
+import java.time.LocalDate;
+
+public record RoleUpdateServiceRequestDto(
+    String role_name,
+    String costume,
+    Boolean sex,
+    LocalDate role_age,
+    Integer limit_personnel,
+    Integer current_personnel,
+    Season season
+)
+{
+
+}
