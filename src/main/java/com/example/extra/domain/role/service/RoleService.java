9a4d6e234768a79c6c86cbb63902ade05d15daac
pangseon <chaosen0521@gmail.com>
2024-08-03 22:23:09 +0900

    feature: role 업데이트 메서드 작성

--- src/main/java/com/example/extra/domain/role/service/RoleService.java
+++ src/main/java/com/example/extra/domain/role/service/RoleService.java
@@ -1,8 +1,17 @@
 package com.example.extra.domain.role.service;
 
+import com.example.extra.domain.company.entity.Company;
 import com.example.extra.domain.role.dto.service.RoleCreateServiceRequestDto;
+import com.example.extra.domain.role.dto.service.RoleUpdateServiceRequestDto;
 
 public interface RoleService {
-    void createRole(Long jobPost_id, RoleCreateServiceRequestDto roleCreateServiceRequestDto);
-
+    void createRole(
+        Long jobPost_id,
+        Company company,
+        RoleCreateServiceRequestDto roleCreateServiceRequestDto);
+    void updateRole(
+        Long jobPost_id,
+        Long role_id,
+        Company company,
+        RoleUpdateServiceRequestDto roleUpdateServiceRequestDto);
 }
