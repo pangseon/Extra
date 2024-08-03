27b41580630f0a654915eb0345fd3355508054fb
hyeneung <imasaru22@gmail.com>
2024-08-04 03:34:30 +0900

    feature: 출연자 의상 컨펌 단건 조회 기능 정의 및 구현

--- src/main/java/com/example/extra/domain/costumeapprovalboard/service/CostumeApprovalBoardService.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/service/CostumeApprovalBoardService.java
@@ -1,5 +1,11 @@
 package com.example.extra.domain.costumeapprovalboard.service;
 
-public interface CostumeApprovalBoardService {
+import com.example.extra.domain.costumeapprovalboard.dto.service.CostumeApprovalBoardMemberReadServiceResponseDto;
+import com.example.extra.domain.member.entity.Member;
 
+public interface CostumeApprovalBoardService {
+    CostumeApprovalBoardMemberReadServiceResponseDto getCostumeApprovalBoardForMember(
+        Member member,
+        Long roleId
+    );
 }
--- src/main/java/com/example/extra/domain/costumeapprovalboard/service/impl/CostumeApprovalBoardServiceImpl.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/service/impl/CostumeApprovalBoardServiceImpl.java
@@ -1,11 +1,29 @@
 package com.example.extra.domain.costumeapprovalboard.service.impl;
 
+import com.example.extra.domain.costumeapprovalboard.dto.service.CostumeApprovalBoardMemberReadServiceResponseDto;
+import com.example.extra.domain.costumeapprovalboard.entity.CostumeApprovalBoard;
+import com.example.extra.domain.costumeapprovalboard.exception.CostumeApprovalBoardErrorCode;
+import com.example.extra.domain.costumeapprovalboard.exception.CostumeApprovalBoardException;
+import com.example.extra.domain.costumeapprovalboard.repository.CostumeApprovalBoardRepository;
 import com.example.extra.domain.costumeapprovalboard.service.CostumeApprovalBoardService;
+import com.example.extra.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class CostumeApprovalBoardServiceImpl implements CostumeApprovalBoardService {
-
+    private final CostumeApprovalBoardRepository costumeApprovalBoardRepository;
+    @Transactional(readOnly = true)
+    public CostumeApprovalBoardMemberReadServiceResponseDto getCostumeApprovalBoardForMember(
+        Member member,
+        Long roleId
+    ){
+        CostumeApprovalBoard costumeApprovalBoard = costumeApprovalBoardRepository.findByMemberAndRoleId(member, roleId)
+            .orElseThrow(() -> new CostumeApprovalBoardException(
+                CostumeApprovalBoardErrorCode.NOT_FOUND_COSTUME_APPROVAL_BOARD_MEMBER)
+            );
+        return CostumeApprovalBoardMemberReadServiceResponseDto.from(costumeApprovalBoard);
+    }
 }
