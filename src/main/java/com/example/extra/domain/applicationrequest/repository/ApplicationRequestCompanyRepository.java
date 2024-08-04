d360ef472fc4a3cf5f58002384e149dbf1306c79
shin0112 <wnslcosltimo12@gmail.com>
2024-08-04 14:51:38 +0900

    feature: repository에 로직 추가
    - member, role -> application request member | company

--- src/main/java/com/example/extra/domain/applicationrequest/repository/ApplicationRequestCompanyRepository.java
+++ src/main/java/com/example/extra/domain/applicationrequest/repository/ApplicationRequestCompanyRepository.java
@@ -1,9 +1,16 @@
 package com.example.extra.domain.applicationrequest.repository;
 
 import com.example.extra.domain.applicationrequest.entity.ApplicationRequestCompany;
+import com.example.extra.domain.member.entity.Member;
+import com.example.extra.domain.role.entity.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicationRequestCompanyRepository extends
     JpaRepository<ApplicationRequestCompany, Long> {
 
+    Optional<ApplicationRequestCompany> findByMemberAndRole(
+        Member member,
+        Role role
+    );
 }
