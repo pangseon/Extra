d2fce16901b1622d94b1b73ad18ab4660e6670f6
hyeneung <imasaru22@gmail.com>
2024-08-04 16:32:47 +0900

    feature: 읽기 권한 여부에 따른 예외 처리

--- src/main/java/com/example/extra/domain/costumeapprovalboard/exception/CostumeApprovalBoardErrorCode.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/exception/CostumeApprovalBoardErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum CostumeApprovalBoardErrorCode implements ErrorCode {
-    ;
+    NOT_ABLE_TO_READ_COSTUME_APPROVAL_BOARD(HttpStatus.BAD_REQUEST, "해당 글을 읽을 권한이 없습니다"),
+    NOT_FOUND_COSTUME_APPROVAL_BOARD(HttpStatus.NOT_FOUND, "유효하지 않은 의상 승인 게시글입니다");
 
     private final HttpStatus status;
     private final String message;
