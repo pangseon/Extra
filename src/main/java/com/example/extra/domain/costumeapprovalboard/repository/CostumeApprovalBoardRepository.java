08e4a3219afb42aede0a26493960a646530d0f70
shin0112 <wnslcosltimo12@gmail.com>
2024-08-04 16:17:42 +0900

    feature: repository 작성
    - member, role -> costume approval board

--- src/main/java/com/example/extra/domain/costumeapprovalboard/repository/CostumeApprovalBoardRepository.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/repository/CostumeApprovalBoardRepository.java
@@ -1,8 +1,15 @@
 package com.example.extra.domain.costumeapprovalboard.repository;
 
 import com.example.extra.domain.costumeapprovalboard.entity.CostumeApprovalBoard;
+import com.example.extra.domain.member.entity.Member;
+import com.example.extra.domain.role.entity.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CostumeApprovalBoardRepository extends JpaRepository<CostumeApprovalBoard, Long> {
 
+    Optional<CostumeApprovalBoard> findByMemberAndRole(
+        Member member,
+        Role role
+    );
 }
