1c07e6ed1d57662d8a54496dc28ff7670a4b44fa
hyeneung <imasaru22@gmail.com>
2024-08-04 16:26:43 +0900

    style: 출연자 단건 조회 dto 필드명 변경 explain->image_explain

---
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/dto/service/CostumeApprovalBoardMemberReadServiceResponseDto.java
@@ -0,0 +1,26 @@
+package com.example.extra.domain.costumeapprovalboard.dto.service;
+
+import com.example.extra.domain.costumeapprovalboard.entity.CostumeApprovalBoard;
+import lombok.Builder;
+
+@Builder
+public record CostumeApprovalBoardMemberReadServiceResponseDto(
+    String role_name,
+    Boolean sex,
+    String costume,
+    String image_explain,
+    String image_url,
+    Boolean costume_approve
+) {
+    public static CostumeApprovalBoardMemberReadServiceResponseDto from(CostumeApprovalBoard costumeApprovalBoard) {
+        return CostumeApprovalBoardMemberReadServiceResponseDto
+            .builder()
+                .role_name(costumeApprovalBoard.getRole().getRole_name())
+                .sex(costumeApprovalBoard.getRole().getSex())
+                .costume(costumeApprovalBoard.getRole().getCostume())
+                .image_explain(costumeApprovalBoard.getImage_explain())
+                .image_url(costumeApprovalBoard.getCostume_image_url())
+                .costume_approve(costumeApprovalBoard.getCostume_approve())
+            .build();
+    }
+}
