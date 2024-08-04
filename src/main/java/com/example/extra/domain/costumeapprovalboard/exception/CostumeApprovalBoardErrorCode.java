9ef9cd61149a530255b4484162f25c171f6dcd9f
hyeneung <imasaru22@gmail.com>
2024-08-04 15:26:10 +0900

    feature: exception 코드 추가

--- src/main/java/com/example/extra/domain/costumeapprovalboard/exception/CostumeApprovalBoardErrorCode.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/exception/CostumeApprovalBoardErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CostumeApprovalBoardErrorCode implements ErrorCode {
 
-    NOT_FOUND_COSTUME_APPROVAL_BOARD_MEMBER(HttpStatus.NOT_FOUND, "의상 컨펌 작성 이력이 없습니다."),;
+    NOT_ABLE_TO_DELETE_COSTUME_APPROVAL_BOARD(HttpStatus.BAD_REQUEST, "해당 글을 삭제할 권한이 없습니다"),
+
+    NOT_FOUND_COSTUME_APPROVAL_BOARD(HttpStatus.NOT_FOUND, "유효하지 않은 의상 승인 게시글입니다");
 
     private final HttpStatus status;
     private final String message;
