0a11398645602e734816bd34fbf5172d993d9485
pangseon <chaosen0521@gmail.com>
2024-08-04 12:22:06 +0900

    feature : Schedule update를 위한 메서드 및 로직 작성

--- src/main/java/com/example/extra/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/example/extra/domain/schedule/service/ScheduleService.java
@@ -2,11 +2,17 @@ package com.example.extra.domain.schedule.service;
 
 import com.example.extra.domain.company.entity.Company;
 import com.example.extra.domain.schedule.dto.service.request.ScheduleCreateServiceRequestDto;
+import com.example.extra.domain.schedule.dto.service.request.ScheduleUpdateServiceRequestDto;
 
 public interface ScheduleService {
     void createSchedule(
-        Long JobPost_id,
+        Long jobPost_id,
         Company company,
         ScheduleCreateServiceRequestDto scheduleCreateServiceRequestDto);
-
+    void updateSchedule(
+        Long jobPost_id,
+        Long schedule_id,
+        Company company,
+        ScheduleUpdateServiceRequestDto scheduleUpdateServiceRequestDto
+    );
 }
--- src/main/java/com/example/extra/domain/schedule/service/impl/ScheduleServiceImpl.java
+++ src/main/java/com/example/extra/domain/schedule/service/impl/ScheduleServiceImpl.java
@@ -7,7 +7,10 @@ import com.example.extra.domain.jobpost.exception.JobPostErrorCode;
 import com.example.extra.domain.jobpost.exception.NotFoundJobPostException;
 import com.example.extra.domain.jobpost.repository.JobPostRepository;
 import com.example.extra.domain.schedule.dto.service.request.ScheduleCreateServiceRequestDto;
+import com.example.extra.domain.schedule.dto.service.request.ScheduleUpdateServiceRequestDto;
 import com.example.extra.domain.schedule.entity.Schedule;
+import com.example.extra.domain.schedule.exception.NotFoundScheduleException;
+import com.example.extra.domain.schedule.exception.ScheduleErrorCode;
 import com.example.extra.domain.schedule.mapper.service.ScheduleEntityMapper;
 import com.example.extra.domain.schedule.repository.ScheduleRepository;
 import com.example.extra.domain.schedule.service.ScheduleService;
@@ -36,6 +39,19 @@ public class ScheduleServiceImpl implements ScheduleService {
         Schedule schedule = scheduleEntityMapper.toSchedule(scheduleCreateServiceRequestDto,jobPost);
         scheduleRepository.save(schedule);
 
+    }
+    public void updateSchedule(
+        Long jobPost_id,
+        Long schedule_id,
+        Company company,
+        ScheduleUpdateServiceRequestDto scheduleUpdateServiceRequestDto
+    ){
+        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,company)
+            .orElseThrow(()->new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
+        Schedule schedule = scheduleRepository.findByIdAndJobPost(schedule_id,jobPost)
+            .orElseThrow(()->new NotFoundScheduleException(ScheduleErrorCode.NOT_FOUND_SCHEDULE));
+        schedule.updateSchedule(scheduleUpdateServiceRequestDto.calender());
+        scheduleRepository.save(schedule);
     }
 
 }
