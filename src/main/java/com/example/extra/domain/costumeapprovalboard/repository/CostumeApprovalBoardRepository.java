f60348e0dc8c28bfd39fe1cc9e3d05d0abc29f8b
hyeneung <imasaru22@gmail.com>
2024-08-04 18:38:47 +0900

    feature: repository 필요 메서드 작성

--- src/main/java/com/example/extra/domain/costumeapprovalboard/repository/CostumeApprovalBoardRepository.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/repository/CostumeApprovalBoardRepository.java
@@ -1,8 +1,11 @@
 package com.example.extra.domain.costumeapprovalboard.repository;
 
 import com.example.extra.domain.costumeapprovalboard.entity.CostumeApprovalBoard;
+import com.example.extra.domain.role.entity.Role;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CostumeApprovalBoardRepository extends JpaRepository<CostumeApprovalBoard, Long> {
-
+    Optional<List<CostumeApprovalBoard>> findAllByRole(Role role);
 }
