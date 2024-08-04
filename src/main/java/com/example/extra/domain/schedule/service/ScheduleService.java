b103a18d6f1ff47af535fc19d78cc1e1e4125ee0
pangseon <chaosen0521@gmail.com>
2024-08-04 13:37:14 +0900

    feature : schedule 데이터 전체조회 및 단건조회를 위한 로직 및 메서드 작성

--- src/main/java/com/example/extra/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/example/extra/domain/schedule/service/ScheduleService.java
@@ -3,6 +3,8 @@ package com.example.extra.domain.schedule.service;
 import com.example.extra.domain.company.entity.Company;
 import com.example.extra.domain.schedule.dto.service.request.ScheduleCreateServiceRequestDto;
 import com.example.extra.domain.schedule.dto.service.request.ScheduleUpdateServiceRequestDto;
+import com.example.extra.domain.schedule.dto.service.response.ScheduleServiceResponseDto;
+import java.util.List;
 
 public interface ScheduleService {
     void createSchedule(
@@ -20,4 +22,9 @@ public interface ScheduleService {
         Long schedule_id,
         Company company
     );
+    ScheduleServiceResponseDto readSchedule(
+        Long jobPost_id,
+        Long schedule_id
+    );
+    List<ScheduleServiceResponseDto> readAllSchedule(Long jobPost_id);
 }
