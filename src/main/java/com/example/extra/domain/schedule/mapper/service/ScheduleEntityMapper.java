b40de8d7bdb285afd9c32762a9c5e3c6a12b87c3
pangseon <chaosen0521@gmail.com>
2024-08-04 13:36:58 +0900

    feature : schedule -> Response Dto 매핑 메서드 작성

--- src/main/java/com/example/extra/domain/schedule/mapper/service/ScheduleEntityMapper.java
+++ src/main/java/com/example/extra/domain/schedule/mapper/service/ScheduleEntityMapper.java
@@ -4,7 +4,9 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 import com.example.extra.domain.jobpost.entity.JobPost;
 import com.example.extra.domain.schedule.dto.service.request.ScheduleCreateServiceRequestDto;
+import com.example.extra.domain.schedule.dto.service.response.ScheduleServiceResponseDto;
 import com.example.extra.domain.schedule.entity.Schedule;
+import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,7 +15,6 @@ public interface ScheduleEntityMapper {
 
     @Mapping(source = "jobPost",target ="jobPost")
     Schedule toSchedule(ScheduleCreateServiceRequestDto scheduleCreateServiceRequestDto, JobPost jobPost);
-/*    @Mapping(source = "company.name",target = "company_name")
-    JobPostServiceResponseDto toJobPostServiceResponseDto(JobPost jobPost, Company company);
-    List<JobPostServiceResponseDto> toListJobPostServiceResponseDto(List<JobPost> jobPostList);*/
+    ScheduleServiceResponseDto toScheduleServiceResponseDto(Schedule schedule);
+    List<ScheduleServiceResponseDto> toListScheduleServiceResponseDto(List<Schedule> scheduleList);
 }
