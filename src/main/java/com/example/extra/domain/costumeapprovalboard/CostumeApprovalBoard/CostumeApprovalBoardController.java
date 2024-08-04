35d0bb4d248521f370c7882f9846d1ad63e953b7
shin0112 <wnslcosltimo12@gmail.com>
2024-08-04 20:23:32 +0900

    feature: controller 의상 승인 게시판 글 수정 틀 작성(회원)

--- src/main/java/com/example/extra/domain/costumeapprovalboard/CostumeApprovalBoard/CostumeApprovalBoardController.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/CostumeApprovalBoard/CostumeApprovalBoardController.java
@@ -1,11 +1,14 @@
 package com.example.extra.domain.costumeapprovalboard.CostumeApprovalBoard;
 
 import com.example.extra.domain.costumeapprovalboard.dto.controller.CostumeApprovalBoardExplainCreateRequestDto;
+import com.example.extra.domain.costumeapprovalboard.dto.controller.CostumeApprovalBoardExplainUpdateControllerRequestDto;
 import com.example.extra.domain.costumeapprovalboard.dto.service.CostumeApprovalBoardCreateServiceDto;
+import com.example.extra.domain.costumeapprovalboard.dto.service.CostumeApprovalBoardExplainUpdateServiceRequestDto;
 import com.example.extra.domain.costumeapprovalboard.mapper.dto.CostumeApprovalBoardDtoMapper;
 import com.example.extra.domain.costumeapprovalboard.service.CostumeApprovalBoardService;
 import com.example.extra.domain.member.entity.Member;
 import com.example.extra.global.security.UserDetailsImpl;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,6 +62,37 @@ public class CostumeApprovalBoardController {
 
         return ResponseEntity
             .status(HttpStatus.CREATED)
+            .build();
+    }
+
+    /**
+     * [회원] 의상 승인 게시판 글 내용 수정하기
+     *
+     * @param userDetails
+     * @param costumeApprovalBoardId
+     * @param controllerRequestDto   : image_explain - Nullable
+     * @param multipartFile          : image file - Nullable
+     * @return ResponseEntity - OK(200)
+     */
+    @PutMapping("/{costume_approval_board_id}/members")
+    public ResponseEntity<?> updateCostumeApprovalBoardByMember(
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PathVariable(name = "costume_approval_board_id") Long costumeApprovalBoardId,
+        @Nullable @RequestPart(name = "explain") CostumeApprovalBoardExplainUpdateControllerRequestDto controllerRequestDto,
+        @Nullable @RequestPart(name = "image") MultipartFile multipartFile
+    ) {
+        CostumeApprovalBoardExplainUpdateServiceRequestDto serviceRequestDto =
+            costumeApprovalBoardDtoMapper.toCostumeApprovalBoardExplainUpdateServiceRequestDto(
+                controllerRequestDto,
+                multipartFile
+            );
+        costumeApprovalBoardService.updateCostumeApprovalBoardByMember(
+            costumeApprovalBoardId,
+            userDetails.getMember(),
+            serviceRequestDto
+        );
+        return ResponseEntity
+            .status(HttpStatus.OK)
             .build();
     }
 }
