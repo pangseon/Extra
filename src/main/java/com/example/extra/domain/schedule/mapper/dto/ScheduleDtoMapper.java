a1361068093e0c4293b8f1ca46ad6ed5d916c040
pangseon <chaosen0521@gmail.com>
2024-08-04 12:00:42 +0900

    feature: Controller -> Service Dto 변환 메서드 작성

--- src/main/java/com/example/extra/domain/schedule/mapper/dto/ScheduleDtoMapper.java
+++ src/main/java/com/example/extra/domain/schedule/mapper/dto/ScheduleDtoMapper.java
@@ -2,6 +2,8 @@ package com.example.extra.domain.schedule.mapper.dto;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
+import com.example.extra.domain.jobpost.dto.controller.JobPostUpdateControllerRequestDto;
+import com.example.extra.domain.jobpost.dto.service.request.JobPostUpdateServiceRequestDto;
 import com.example.extra.domain.schedule.dto.controller.ScheduleCreateControllerRequestDto;
 import com.example.extra.domain.schedule.dto.service.request.ScheduleCreateServiceRequestDto;
 import org.mapstruct.Mapper;
@@ -12,8 +14,8 @@ public interface ScheduleDtoMapper {
     ScheduleCreateServiceRequestDto toScheduleCreateServiceDto(
       ScheduleCreateControllerRequestDto scheduleCreateControllerRequestDto
     );
-/*    JobPostUpdateServiceRequestDto toJobPostUpdateServiceDto(
+    JobPostUpdateServiceRequestDto toJobPostUpdateServiceDto(
         JobPostUpdateControllerRequestDto jobPostUpdateControllerRequestDto
-    );*/
+    );
 
 }
