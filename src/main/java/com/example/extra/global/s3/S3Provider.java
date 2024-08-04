1efab6ca7eb05d82bd725f9409ed514bb5b16376
pangseon <chaosen0521@gmail.com>
2024-08-04 20:46:55 +0900

    feature : multipartfile 파일 이름 추출 후 재할당

---
+++ src/main/java/com/example/extra/global/s3/S3Provider.java
@@ -0,0 +1,51 @@
+package com.example.extra.global.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+@RequiredArgsConstructor
+public class S3Provider {
+    private final AmazonS3 amazonS3;
+    public static final String SEPARATOR = "/";
+    @Value("${cloud.aws.s3.bucket.name}")
+    public String bucket;
+    public final String url = "https://extra-file.s3.ap-northeast-2.amazonaws.com/";
+
+    public void createFolder(String folderName) {
+        if (!amazonS3.doesObjectExist(bucket, folderName)) {
+            amazonS3.putObject(
+                bucket,
+                folderName + SEPARATOR,
+                new ByteArrayInputStream(new byte[0]),
+                new ObjectMetadata());
+        }
+    }
+    public String originalFileName(MultipartFile multipartFile) {
+        if (multipartFile.isEmpty()) {
+            return "";
+        }
+        if (Objects.equals(multipartFile.getContentType(), "image/png")
+            || Objects.equals(multipartFile.getContentType(), "image/jpeg")) {
+            String fileType = switch (multipartFile.getContentType()) {
+                case "image/png" -> ".png";
+                case "image/jpeg" -> ".jpg";
+                default -> throw new IllegalStateException(
+                    "Unexpected value: " + multipartFile.getContentType());
+            };
+            return UUID.randomUUID() + fileType;
+        } else {
+            throw new IllegalArgumentException("잘못된 파일 형식입니다");
+        }
+    }
+
+
+}
