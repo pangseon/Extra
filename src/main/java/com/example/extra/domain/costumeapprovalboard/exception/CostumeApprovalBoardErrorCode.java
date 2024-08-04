86bb9589fae55eca557ab338e8135e301194d627
hyeneung <imasaru22@gmail.com>
2024-08-04 18:38:08 +0900

    feature: 접근 권한 예외 처리

--- src/main/java/com/example/extra/domain/costumeapprovalboard/exception/CostumeApprovalBoardErrorCode.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/exception/CostumeApprovalBoardErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum CostumeApprovalBoardErrorCode implements ErrorCode {
-    ;
+    NOT_ABLE_TO_ACCESS_COSTUME_APPROVAL_BOARD(HttpStatus.BAD_REQUEST, "해당 공고에 대한 권한이 없습니다");
 
     private final HttpStatus status;
     private final String message;
