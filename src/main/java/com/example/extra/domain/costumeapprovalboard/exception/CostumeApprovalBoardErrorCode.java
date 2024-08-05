3ed9bb6af803e3154d74f48d802db03ac1a85c0e
shin0112 <wnslcosltimo12@gmail.com>
2024-08-05 19:27:52 +0900

    feature: error code 추가

--- src/main/java/com/example/extra/domain/costumeapprovalboard/exception/CostumeApprovalBoardErrorCode.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/exception/CostumeApprovalBoardErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum CostumeApprovalBoardErrorCode implements ErrorCode {
     // 400
     ALREADY_EXIST_COSTUME_APPROVAL_BOARD(HttpStatus.BAD_REQUEST, "이미 글을 작성했습니다."),
+    ALREADY_APPROVED(HttpStatus.BAD_REQUEST, "이미 승인된 글입니다."),
     NOT_MASTER_COSTUME_APPROVAL_BOARD(HttpStatus.BAD_REQUEST, "회원이 작성한 글이 아닙니다."),
     NOT_ABLE_TO_ACCESS_COSTUME_APPROVAL_BOARD(HttpStatus.BAD_REQUEST, "해당 공고에 대한 권한이 없습니다"),
     NOT_ABLE_TO_READ_COSTUME_APPROVAL_BOARD(HttpStatus.BAD_REQUEST, "해당 글을 읽을 권한이 없습니다"),
