d35243220cb99cedbd9d76ffff2fc923d176fede
hyeneung <imasaru22@gmail.com>
2024-08-06 23:31:01 +0900

    style: 권한 위반에 대한 exception의 error code를 403으로 변경

--- src/main/java/com/example/extra/domain/applicationrequest/exception/ApplicationRequestErrorCode.java
+++ src/main/java/com/example/extra/domain/applicationrequest/exception/ApplicationRequestErrorCode.java
@@ -11,8 +11,10 @@ public enum ApplicationRequestErrorCode implements ErrorCode {
     // 400
     NOT_ABLE_TO_CANCEL_IN_APPROVED(HttpStatus.BAD_REQUEST, "승인 상태에서는 지원 철회가 불가합니다."),
     NOT_ABLE_TO_APPLY_TO_JOB_POST(HttpStatus.BAD_REQUEST, "해당 공고는 마감되었습니다."),
-    NOT_ABLE_TO_ACCESS_APPLICATION_REQUEST_MEMBER(HttpStatus.BAD_REQUEST, "접근 권한이 없습니다"),
     NOT_APPROVED_REQUEST(HttpStatus.BAD_REQUEST, "승인되지 않은 공고입니다."),
+
+    // 403
+    NOT_ABLE_TO_ACCESS_APPLICATION_REQUEST_MEMBER(HttpStatus.FORBIDDEN, "접근 권한이 없습니다"),
 
     // 404
     NOT_FOUND_APPLICATION_REQUEST_MEMBER(HttpStatus.NOT_FOUND, "해당 역할은 더이상 유효하지 않습니다."),
