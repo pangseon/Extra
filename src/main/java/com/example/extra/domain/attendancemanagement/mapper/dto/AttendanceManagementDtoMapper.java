5b65c2147fb4cd1fc0f22083ac27a5b465680eaa
hyeneung <imasaru22@gmail.com>
2024-08-07 01:58:38 +0900

    feature: 식사 qr 체크 용 dto mapper 추가

--- src/main/java/com/example/extra/domain/attendancemanagement/mapper/dto/AttendanceManagementDtoMapper.java
+++ src/main/java/com/example/extra/domain/attendancemanagement/mapper/dto/AttendanceManagementDtoMapper.java
@@ -1,13 +1,22 @@
 package com.example.extra.domain.attendancemanagement.mapper.dto;
 
 import com.example.extra.domain.attendancemanagement.dto.controller.AttendanceManagementUpdateControllerRequestDto;
+import com.example.extra.domain.attendancemanagement.dto.controller.AttendanceManagementUpdateMealCountControllerRequestDto;
 import com.example.extra.domain.attendancemanagement.dto.service.AttendanceManagementUpdateServiceRequestDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = ComponentModel.SPRING)
+@Mapper(
+    componentModel = ComponentModel.SPRING,
+    unmappedTargetPolicy = ReportingPolicy.IGNORE,
+    unmappedSourcePolicy = ReportingPolicy.IGNORE
+)
 public interface AttendanceManagementDtoMapper {
     AttendanceManagementUpdateServiceRequestDto toAttendanceManagementUpdateServiceRequestDto(
         AttendanceManagementUpdateControllerRequestDto controllerRequestDto
+    );
+    AttendanceManagementUpdateServiceRequestDto toAttendanceManagementUpdateServiceRequestDto(
+        AttendanceManagementUpdateMealCountControllerRequestDto controllerRequestDto
     );
 }
