50422f6e157448938af040f5d6b26e0020920aca
hyeneung <imasaru22@gmail.com>
2024-08-06 20:28:49 +0900

    feature: apply status 입력 검증

--- src/main/java/com/example/extra/domain/costumeapprovalboard/dto/controller/CostumeApprovalBoardApplyStatusUpdateControllerRequestDto.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/dto/controller/CostumeApprovalBoardApplyStatusUpdateControllerRequestDto.java
@@ -1,7 +1,10 @@
 package com.example.extra.domain.costumeapprovalboard.dto.controller;
 
+import com.example.extra.global.enums.ApplyStatus;
+import jakarta.validation.constraints.NotNull;
+
 public record CostumeApprovalBoardApplyStatusUpdateControllerRequestDto(
-    String costumeApprove
+    @NotNull ApplyStatus costumeApprove
 ) {
 
 }
--- src/main/java/com/example/extra/domain/costumeapprovalboard/dto/service/CostumeApprovalBoardApplyStatusUpdateServiceRequestDto.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/dto/service/CostumeApprovalBoardApplyStatusUpdateServiceRequestDto.java
@@ -1,7 +1,9 @@
 package com.example.extra.domain.costumeapprovalboard.dto.service;
 
+import com.example.extra.global.enums.ApplyStatus;
+
 public record CostumeApprovalBoardApplyStatusUpdateServiceRequestDto(
-    String costumeApprove
+    ApplyStatus costumeApprove
 ) {
 
 }
