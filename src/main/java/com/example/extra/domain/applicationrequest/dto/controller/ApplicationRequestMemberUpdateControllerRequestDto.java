73771fb33322d02fe48a3dcb3bd71f147ada6143
hyeneung <imasaru22@gmail.com>
2024-08-07 16:36:34 +0900

    style: exception message 수정

--- src/main/java/com/example/extra/domain/applicationrequest/dto/controller/ApplicationRequestMemberUpdateControllerRequestDto.java
+++ src/main/java/com/example/extra/domain/applicationrequest/dto/controller/ApplicationRequestMemberUpdateControllerRequestDto.java
@@ -4,7 +4,7 @@ import com.example.extra.global.enums.ApplyStatus;
 import jakarta.validation.constraints.NotNull;
 
 public record ApplicationRequestMemberUpdateControllerRequestDto(
-    @NotNull(message = "유효하지 않은 apply status입니다.")
+    @NotNull(message = "유효한 apply status를 입력해주세요.")
     ApplyStatus applyStatus
 ) {
 
