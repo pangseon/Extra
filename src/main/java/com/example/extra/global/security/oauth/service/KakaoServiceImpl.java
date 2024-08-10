636bb5f1e95f0aa2468b37a86b86984c612e7dd5
shin0112 <wnslcosltimo12@gmail.com>
2024-08-11 03:13:25 +0900

    feature: kakao login service 작성
    - 인증 코드 받기
    - access token 받기
    - 이메일 유효 검사하기

---
+++ src/main/java/com/example/extra/global/security/oauth/service/KakaoServiceImpl.java
@@ -0,0 +1,135 @@
+package com.example.extra.global.security.oauth.service;
+
+import com.example.extra.domain.member.exception.MemberErrorCode;
+import com.example.extra.domain.member.exception.MemberException;
+import com.example.extra.domain.member.repository.MemberRepository;
+import com.example.extra.global.security.oauth.dto.service.response.KakaoInfoReadServiceResponseDto;
+import com.example.extra.global.security.oauth.entity.KakaoInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoServiceImpl {
+
+    private final MemberRepository memberRepository;
+
+    @Value("${kakao.client.id}")
+    String clientId;
+    @Value("${kakao.redirect.uri}")
+    String redirectUri;
+    @Value("${kakao.client.secret}")
+    String clientSecret;
+
+    public String authorize() {
+        String url = "https://kauth.kakao.com/oauth/authorize" +
+            "client_id=" + clientId +
+            "&redirect_uri=" + redirectUri +
+            "&response_type=code";
+
+        return "redirect:" + url;
+    }
+
+    public List<String> getToken(String code) throws JsonProcessingException {
+        // HTTP Header
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(
+            "Content-type",
+            "application/x-www-form-urlencoded;charset=utf-8"
+        );
+
+        // HTTP Body
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("client_id", clientId);
+        body.add("redirect_uri", redirectUri);
+        body.add("code", code);
+        body.add("client_secret", clientSecret);
+
+        // HTTP request
+        HttpEntity<MultiValueMap<String, String>> request =
+            new HttpEntity<>(body, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response =
+            restTemplate.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                request,
+                String.class
+            );
+
+        // response - json parsing
+        String responseBody = response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+        // [0] accessToken, [1] refreshToken
+        List<String> tokenList = new ArrayList<>();
+        tokenList.add(jsonNode.get("access_token").asText());
+        tokenList.add(jsonNode.get("refresh_token").asText());
+
+        return tokenList;
+    }
+
+    private void validate(final KakaoInfo kakaoInfo) {
+        // 중복 확인
+        String email = kakaoInfo.getEmail();
+        memberRepository.findByEmail(email)
+            .ifPresent(m -> {
+                throw new MemberException(MemberErrorCode.ALREADY_EXIST_MEMBER);
+            });
+    }
+
+    public KakaoInfoReadServiceResponseDto getKakaoInfo(String accessToken)
+        throws JsonProcessingException {
+        // HTTP Header
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(
+            "Authorization",
+            "Bearer " + accessToken
+        );
+        headers.add(
+            "Content-type",
+            "application/x-www-form-urlencoded;charset=utf-8"
+        );
+
+        // HTTP request
+        HttpEntity<MultiValueMap<String, String>> request =
+            new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response =
+            restTemplate.exchange(
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                request,
+                String.class
+            );
+
+        // response - json parsing
+        String responseBody = response.getBody();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+        String email = jsonNode
+            .get("kakao_account")
+            .get("email")
+            .asText();
+
+        validate(new KakaoInfo(email));
+
+        return new KakaoInfoReadServiceResponseDto(email);
+    }
+}
