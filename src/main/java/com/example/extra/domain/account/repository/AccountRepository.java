546d7662876a0d568741c1e43a4b09b1000bdc85
shin0112 <wnslcosltimo12@gmail.com>
2024-08-13 20:28:48 +0900

    refactor: access token 재발급 로직 수정
    - member & company -> account 대체

--- src/main/java/com/example/extra/domain/account/repository/AccountRepository.java
+++ src/main/java/com/example/extra/domain/account/repository/AccountRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findByEmail(String email);
+
+    Optional<Account> findByRefreshToken(String refreshToken);
 }
