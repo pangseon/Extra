97f0dd4c6075bb0c815c3dd7c5b52064fa2f8814
shin0112 <wnslcosltimo12@gmail.com>
2024-08-13 22:30:45 +0900

    refactor: role 파라미터 수정 - userDetails account

--- src/main/java/com/example/extra/domain/role/service/impl/RoleServiceImpl.java
+++ src/main/java/com/example/extra/domain/role/service/impl/RoleServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.extra.domain.role.service.impl;
 
+import com.example.extra.domain.account.entity.Account;
+import com.example.extra.domain.account.exception.AccountErrorCode;
+import com.example.extra.domain.account.exception.AccountException;
 import com.example.extra.domain.company.entity.Company;
+import com.example.extra.domain.company.repository.CompanyRepository;
 import com.example.extra.domain.jobpost.entity.JobPost;
 import com.example.extra.domain.jobpost.exception.JobPostErrorCode;
 import com.example.extra.domain.jobpost.exception.NotFoundJobPostException;
@@ -21,65 +25,78 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class RoleServiceImpl implements RoleService {
+
     private final JobPostRepository jobPostRepository;
     private final RoleEntityMapper roleEntityMapper;
     private final RoleRepository roleRepository;
+    private final CompanyRepository companyRepository;
+
+    private Company getCompanyByAccount(final Account account) {
+        return companyRepository.findByAccount(account)
+            .orElseThrow(() -> new AccountException(AccountErrorCode.NOT_FOUND_ACCOUNT));
+    }
 
     public void createRole(
         Long jobPost_id,
-        Company company,
-        RoleCreateServiceRequestDto roleCreateServiceRequestDto)
-    {
-        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,company)
-            .orElseThrow(()->new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
-        Role role = roleEntityMapper.toRole(roleCreateServiceRequestDto,jobPost);
+        final Account account,
+        RoleCreateServiceRequestDto roleCreateServiceRequestDto) {
+        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,
+                getCompanyByAccount(account))
+            .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
+        Role role = roleEntityMapper.toRole(roleCreateServiceRequestDto, jobPost);
         roleRepository.save(role);
     }
+
     public void updateRole(
         Long jobPost_id,
         Long role_id,
-        Company company,
+        final Account account,
         RoleUpdateServiceRequestDto roleUpdateServiceRequestDto
-    ){
-        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,company)
-            .orElseThrow(()->new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
-        Role role = roleRepository.findByIdAndJobPost(role_id,jobPost)
-            .orElseThrow(()->new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
+    ) {
+        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,
+                getCompanyByAccount(account))
+            .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
+        Role role = roleRepository.findByIdAndJobPost(role_id, jobPost)
+            .orElseThrow(() -> new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
         role.updateRole(
             roleUpdateServiceRequestDto.roleName(),
             roleUpdateServiceRequestDto.costume(),
             roleUpdateServiceRequestDto.sex(),
-                roleUpdateServiceRequestDto.roleAge(),
+            roleUpdateServiceRequestDto.roleAge(),
             roleUpdateServiceRequestDto.limitPersonnel(),
             roleUpdateServiceRequestDto.currentPersonnel(),
             roleUpdateServiceRequestDto.season(),
             roleUpdateServiceRequestDto.checkTattoo());
         roleRepository.save(role);
     }
+
     public void deleteRole(
         Long jobPost_id,
         Long role_id,
-        Company company
-    ){
-        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,company)
-            .orElseThrow(()->new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
-        Role role = roleRepository.findByIdAndJobPost(role_id,jobPost)
-            .orElseThrow(()->new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
+        final Account account
+    ) {
+        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,
+                getCompanyByAccount(account))
+            .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
+        Role role = roleRepository.findByIdAndJobPost(role_id, jobPost)
+            .orElseThrow(() -> new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
         roleRepository.delete(role);
     }
+
     public RoleServiceResponseDto readRole(
         Long jobPost_id,
         Long role_id
-    ){
+    ) {
         JobPost jobPost = jobPostRepository.findById(jobPost_id)
-            .orElseThrow(()->new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
-        Role role = roleRepository.findByIdAndJobPost(role_id,jobPost)
-            .orElseThrow(()->new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
+            .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
+        Role role = roleRepository.findByIdAndJobPost(role_id, jobPost)
+            .orElseThrow(() -> new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
         return roleEntityMapper.toRoleServiceResponseDto(role);
     }
-    public List<RoleServiceResponseDto> readAllRole(Long jobPost_id){
-        JobPost jobPost =jobPostRepository.findById(jobPost_id)
-            .orElseThrow(()->new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
+
+    public List<RoleServiceResponseDto> readAllRole(Long jobPost_id) {
+        JobPost jobPost = jobPostRepository.findById(jobPost_id)
+            .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
         List<Role> roleList = roleRepository.findAllByJobPost(jobPost);
         return roleEntityMapper.toListRoleServiceResponseDto(roleList);
     }
