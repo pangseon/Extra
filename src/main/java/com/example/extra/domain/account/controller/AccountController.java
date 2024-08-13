fcd307753e7c5837ca1bd2484b08431e3c16864f
shin0112 <wnslcosltimo12@gmail.com>
2024-08-13 19:14:58 +0900

    feature: account controller 수정 - sign up 작성

--- src/main/java/com/example/extra/domain/account/controller/AccountController.java
+++ src/main/java/com/example/extra/domain/account/controller/AccountController.java
@@ -1,8 +1,13 @@
 package com.example.extra.domain.account.controller;
 
+import com.example.extra.domain.account.dto.controller.AccountCreateControllerRequestDto;
+import com.example.extra.domain.account.dto.service.request.AccountCreateServiceRequestDto;
 import com.example.extra.domain.account.mapper.dto.AccountDtoMapper;
 import com.example.extra.domain.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api/v1/account")
@@ -13,4 +18,17 @@ public class AccountController {
 
     // mapper
     private final AccountDtoMapper accountDtoMapper;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(AccountCreateControllerRequestDto controllerRequestDto) {
+        AccountCreateServiceRequestDto serviceRequestDto =
+            accountDtoMapper.toAccountCreateServiceRequestDto(controllerRequestDto);
+
+        accountService.signup(serviceRequestDto);
+        
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .build();
+    }
+
 }
