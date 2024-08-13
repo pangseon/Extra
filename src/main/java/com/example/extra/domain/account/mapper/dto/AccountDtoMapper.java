d48ed621e939738b51fff47ddd8242d6fb96a538
shin0112 <wnslcosltimo12@gmail.com>
2024-08-13 20:49:23 +0900

    feature: login dto mapper 작성

--- src/main/java/com/example/extra/domain/account/mapper/dto/AccountDtoMapper.java
+++ src/main/java/com/example/extra/domain/account/mapper/dto/AccountDtoMapper.java
@@ -3,7 +3,9 @@ package com.example.extra.domain.account.mapper.dto;
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
 import com.example.extra.domain.account.dto.controller.AccountCreateControllerRequestDto;
+import com.example.extra.domain.account.dto.controller.AccountLoginControllerRequestDto;
 import com.example.extra.domain.account.dto.service.request.AccountCreateServiceRequestDto;
+import com.example.extra.domain.account.dto.service.request.AccountLoginServiceRequestDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = SPRING)
@@ -11,5 +13,9 @@ public interface AccountDtoMapper {
 
     AccountCreateServiceRequestDto toAccountCreateServiceRequestDto(
         AccountCreateControllerRequestDto controllerRequestDto
+    );
+
+    AccountLoginServiceRequestDto toAccountLoginServiceRequestDto(
+        AccountLoginControllerRequestDto controllerRequestDto
     );
 }
