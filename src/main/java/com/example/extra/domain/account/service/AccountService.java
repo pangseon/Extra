91243fb03bb42478b626d239a5f7d7574636f37e
shin0112 <wnslcosltimo12@gmail.com>
2024-08-13 19:25:47 +0900

    feature: account service - sign up 로직 작성

--- src/main/java/com/example/extra/domain/account/service/AccountService.java
+++ src/main/java/com/example/extra/domain/account/service/AccountService.java
@@ -1,5 +1,9 @@
 package com.example.extra.domain.account.service;
 
+import com.example.extra.domain.account.dto.service.request.AccountCreateServiceRequestDto;
+import com.example.extra.domain.account.dto.service.response.AccountCreateServiceResponseDto;
+
 public interface AccountService {
 
+    AccountCreateServiceResponseDto signup(AccountCreateServiceRequestDto serviceRequestDto);
 }
--- src/main/java/com/example/extra/domain/account/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/extra/domain/account/service/impl/AccountServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.extra.domain.account.service.impl;
 
+import com.example.extra.domain.account.dto.service.request.AccountCreateServiceRequestDto;
+import com.example.extra.domain.account.dto.service.response.AccountCreateServiceResponseDto;
+import com.example.extra.domain.account.entity.Account;
 import com.example.extra.domain.account.mapper.entity.AccountEntityMapper;
 import com.example.extra.domain.account.repository.AccountRepository;
 import com.example.extra.domain.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +18,14 @@ public class AccountServiceImpl implements AccountService {
 
     // mapper
     private final AccountEntityMapper accountEntityMapper;
+
+    @Override
+    @Transactional
+    public AccountCreateServiceResponseDto signup(
+        final AccountCreateServiceRequestDto serviceRequestDto
+    ) {
+        Account account = accountEntityMapper.toAccount(serviceRequestDto);
+
+        return new AccountCreateServiceResponseDto(accountRepository.save(account).getId());
+    }
 }
