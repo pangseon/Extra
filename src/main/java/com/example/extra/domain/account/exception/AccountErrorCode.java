3679138bda2ad3328ab05c15fefb2095f80c515d
shin0112 <wnslcosltimo12@gmail.com>
2024-08-13 20:04:28 +0900

    feature: 회사 회원 가입 error code & mapper 변경
    - error code 추가
    - comapny entity mapper에 account 추가

--- src/main/java/com/example/extra/domain/account/exception/AccountErrorCode.java
+++ src/main/java/com/example/extra/domain/account/exception/AccountErrorCode.java
@@ -10,8 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum AccountErrorCode implements ErrorCode {
     // 400
     INVALID_ROLE_USER(HttpStatus.BAD_REQUEST, "개인 회원 회원가입을 해주세요"),
+    INVALID_ROLE_COMPANY(HttpStatus.BAD_REQUEST, "회사 회원가입을 해주세요"),
     DUPLICATION_EMAIL(HttpStatus.BAD_REQUEST, "중복된 아이디"),
-    
+
     // 404
     NOT_FOUND_ACCOUNT(HttpStatus.NOT_FOUND, "존재하지 않은 게정입니다"),
     ;
