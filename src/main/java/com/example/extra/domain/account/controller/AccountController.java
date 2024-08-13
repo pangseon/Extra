f494750b159bf576470ac66d2b5b6e2136f522b7
shin0112 <wnslcosltimo12@gmail.com>
2024-08-13 22:44:12 +0900

    bug: forbidden 해결
    - request mapping url 작성
    - security 통과 처리

--- src/main/java/com/example/extra/domain/account/controller/AccountController.java
+++ src/main/java/com/example/extra/domain/account/controller/AccountController.java
@@ -10,9 +10,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/v1/account")
+@RestController
+@RequestMapping("api/v1/account")
 @RequiredArgsConstructor
 public class AccountController {
 
