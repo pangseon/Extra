6696ef1d6c041628535abcf1d1a86b5665f58ddb
pangseon <chaosen0521@gmail.com>
2024-08-14 18:45:33 +0900

    Feature : 타투 생성 기능 추가

---
+++ src/main/java/com/example/extra/domain/tattoo/service/impl/TattooServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.extra.domain.tattoo.service.impl;
+
+
+import com.example.extra.domain.tattoo.dto.service.request.TattooCreateServiceRequestDto;
+import com.example.extra.domain.tattoo.entity.Tattoo;
+import com.example.extra.domain.tattoo.mapper.service.TattooEntityMapper;
+import com.example.extra.domain.tattoo.repository.TattooRepository;
+import com.example.extra.domain.tattoo.service.TattooService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TattooServiceImpl implements TattooService {
+
+    private final TattooRepository tattooRepository;
+    private final TattooEntityMapper tattooEntityMapper;
+    @Override
+    public void create(final TattooCreateServiceRequestDto requestDto) {
+        Tattoo tattoo = tattooEntityMapper.toTattoo(requestDto);
+        tattooRepository.save(tattoo);
+    }
+}
