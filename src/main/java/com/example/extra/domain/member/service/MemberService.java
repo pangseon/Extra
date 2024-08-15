8cf6c2a4181f4e29bb9ef04d10d87bdcab265e66
shin0112 <wnslcosltimo12@gmail.com>
2024-08-15 23:24:13 +0900

    feature: service - 회원 수정 로직
    - 타투 찾기
    - member 정보 update
    - account의 image url 수정

--- src/main/java/com/example/extra/domain/member/service/MemberService.java
+++ src/main/java/com/example/extra/domain/member/service/MemberService.java
@@ -1,10 +1,14 @@
 package com.example.extra.domain.member.service;
 
+import com.example.extra.domain.account.entity.Account;
 import com.example.extra.domain.member.dto.service.request.MemberCreateServiceRequestDto;
+import com.example.extra.domain.member.dto.service.request.MemberUpdateServiceRequestDto;
 import com.example.extra.domain.member.dto.service.response.MemberReadServiceResponseDto;
 import com.example.extra.domain.tattoo.dto.service.request.TattooCreateServiceRequestDto;
 import com.example.extra.global.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberService {
 
@@ -22,4 +26,11 @@ public interface MemberService {
         final UserDetailsImpl userDetails,
         final HttpServletRequest request
     );
+
+    void update(
+        Account account,
+        MemberUpdateServiceRequestDto memberUpdateServiceRequestDto,
+        TattooCreateServiceRequestDto tattooCreateServiceRequestDto,
+        MultipartFile multipartFile
+    ) throws IOException;
 }
