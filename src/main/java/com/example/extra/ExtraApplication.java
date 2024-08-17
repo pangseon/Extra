e764cc9217682cadb84159b10e27618af713acb3
shin0112 <wnslcosltimo12@gmail.com>
2024-08-17 21:24:36 +0900

    feature: swagger api endpoint 변경

--- src/main/java/com/example/extra/ExtraApplication.java
+++ src/main/java/com/example/extra/ExtraApplication.java
@@ -1,10 +1,15 @@
 package com.example.extra;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@OpenAPIDefinition(servers = {
+    @Server(url = "${domain.url}", description = "Default Server URL")}
+)
 @SpringBootApplication
 public class ExtraApplication {
 
