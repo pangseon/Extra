24356d6cda9fad124b562d2b23c719cdea5ef3ef
shin0112 <wnslcosltimo12@gmail.com>
2024-08-18 01:00:33 +0900

    refactor: login response dto 필드 변경

--- src/main/java/com/example/extra/domain/account/dto/service/response/AccountLoginServiceResponseDto.java
+++ src/main/java/com/example/extra/domain/account/dto/service/response/AccountLoginServiceResponseDto.java
@@ -1,7 +1,8 @@
 package com.example.extra.domain.account.dto.service.response;
 
 public record AccountLoginServiceResponseDto(
-    String accessToken
+    String accessToken,
+    String refreshToken
 ) {
 
 }
