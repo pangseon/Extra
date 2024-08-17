28d86d0fa8cb11b832ee230b4ced7a4ff9ada4ce
shin0112 <wnslcosltimo12@gmail.com>
2024-08-17 15:55:21 +0900

    style: response entity 값 추가 & convention 준수

--- src/main/java/com/example/extra/domain/member/controller/MemberController.java
+++ src/main/java/com/example/extra/domain/member/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.example.extra.domain.member.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
 import com.example.extra.domain.member.dto.controller.MemberSignUpControllerRequestDto;
 import com.example.extra.domain.member.dto.controller.MemberUpdateControllerRequestDto;
 import com.example.extra.domain.member.dto.service.request.MemberCreateServiceRequestDto;
@@ -17,7 +20,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,7 +43,7 @@ public class MemberController {
     public static final String AUTHORIZATION_HEADER = "Authorization";
 
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(
+    public ResponseEntity<Void> signup(
         @Valid @RequestBody MemberSignUpControllerRequestDto requestDto
     ) {
         MemberCreateServiceRequestDto memberCreateServiceRequestDto =
@@ -56,7 +58,9 @@ public class MemberController {
             tattooCreateServiceRequestDto
         );
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity
+            .status(CREATED)
+            .build();
     }
 
     @GetMapping("")
@@ -69,11 +73,14 @@ public class MemberController {
                 userDetails,
                 request
             );
-        return ResponseEntity.status(HttpStatus.OK).body(serviceResponseDto);
+
+        return ResponseEntity
+            .status(OK)
+            .body(serviceResponseDto);
     }
 
     @PutMapping("")
-    public ResponseEntity<?> update(
+    public ResponseEntity<Void> update(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @Nullable @RequestPart(name = "member") MemberUpdateControllerRequestDto memberUpdateControllerRequestDto,
         @Nullable @RequestPart(name = "tattoo") TattooCreateControllerRequestDto tattooCreateControllerRequestDto,
@@ -97,12 +104,12 @@ public class MemberController {
         );
 
         return ResponseEntity
-            .status(HttpStatus.OK)
+            .status(OK)
             .build();
     }
 
     @DeleteMapping("/{member_id}")
-    public ResponseEntity<?> deleteUser(
+    public ResponseEntity<Void> deleteUser(
         @AuthenticationPrincipal UserDetailsImpl userDetails,
         @NotNull HttpServletRequest request
     ) {
@@ -110,6 +117,9 @@ public class MemberController {
             userDetails,
             request
         );
-        return ResponseEntity.status(HttpStatus.OK).build();
+
+        return ResponseEntity
+            .status(OK)
+            .build();
     }
 }
