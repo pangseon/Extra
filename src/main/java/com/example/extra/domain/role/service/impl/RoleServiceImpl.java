dca8478600a0f6c1c6280a2dc82e9719b814cc65
hyeneung <imasaru22@gmail.com>
2024-08-17 18:50:26 +0900

    feature: role service 수정

--- src/main/java/com/example/extra/domain/role/service/impl/RoleServiceImpl.java
+++ src/main/java/com/example/extra/domain/role/service/impl/RoleServiceImpl.java
@@ -13,16 +13,21 @@ import com.example.extra.domain.role.dto.service.request.RoleCreateServiceReques
 import com.example.extra.domain.role.dto.service.request.RoleUpdateServiceRequestDto;
 import com.example.extra.domain.role.dto.service.response.RoleServiceResponseDto;
 import com.example.extra.domain.role.entity.Role;
-import com.example.extra.domain.role.exception.NotFoundRoleException;
+import com.example.extra.domain.role.exception.RoleException;
 import com.example.extra.domain.role.exception.RoleErrorCode;
 import com.example.extra.domain.role.mapper.service.RoleEntityMapper;
 import com.example.extra.domain.role.repository.RoleRepository;
 import com.example.extra.domain.role.service.RoleService;
+import com.example.extra.domain.tattoo.entity.Tattoo;
+import com.example.extra.domain.tattoo.exception.TattooErrorCode;
+import com.example.extra.domain.tattoo.exception.TattooException;
+import com.example.extra.domain.tattoo.repository.TattooRepository;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -32,34 +37,41 @@ public class RoleServiceImpl implements RoleService {
     private final RoleEntityMapper roleEntityMapper;
     private final RoleRepository roleRepository;
     private final CompanyRepository companyRepository;
+    private final TattooRepository tattooRepository;
 
-    private Company getCompanyByAccount(final Account account) {
-        return companyRepository.findByAccount(account)
-            .orElseThrow(() -> new AccountException(AccountErrorCode.NOT_FOUND_ACCOUNT));
-    }
-
+    @Override
+    @Transactional
     public void createRole(
-        Long jobPost_id,
+        Long jobPostId,
         final Account account,
-        RoleCreateServiceRequestDto roleCreateServiceRequestDto) {
-        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,
+        final RoleCreateServiceRequestDto roleCreateServiceRequestDto
+    ) {
+        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPostId,
                 getCompanyByAccount(account))
             .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
         Role role = roleEntityMapper.toRole(roleCreateServiceRequestDto, jobPost);
+        Tattoo tattoo = tattooRepository.findByTattooCreateControllerRequestDto(
+                roleCreateServiceRequestDto.tattoo())
+            .orElseThrow(() -> new TattooException(TattooErrorCode.NOT_FOUND_TATTOO));
+        role.updateTattoo(tattoo);
         roleRepository.save(role);
     }
-
+    @Override
+    @Transactional
     public void updateRole(
-        Long jobPost_id,
-        Long role_id,
+        Long jobPostId,
+        Long roleId,
         final Account account,
-        RoleUpdateServiceRequestDto roleUpdateServiceRequestDto
+        final RoleUpdateServiceRequestDto roleUpdateServiceRequestDto
     ) {
-        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,
+        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPostId,
                 getCompanyByAccount(account))
             .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
-        Role role = roleRepository.findByIdAndJobPost(role_id, jobPost)
-            .orElseThrow(() -> new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
+        Role role = roleRepository.findByIdAndJobPost(roleId, jobPost)
+            .orElseThrow(() -> new RoleException(RoleErrorCode.NOT_FOUND_ROLE));
+        Tattoo tattoo = tattooRepository.findByTattooCreateControllerRequestDto(
+                roleUpdateServiceRequestDto.tattoo())
+            .orElseThrow(() -> new TattooException(TattooErrorCode.NOT_FOUND_TATTOO));
         role.updateRole(
             roleUpdateServiceRequestDto.roleName(),
             roleUpdateServiceRequestDto.costume(),
@@ -69,49 +81,56 @@ public class RoleServiceImpl implements RoleService {
             roleUpdateServiceRequestDto.limitPersonnel(),
             roleUpdateServiceRequestDto.currentPersonnel(),
             roleUpdateServiceRequestDto.season(),
-            roleUpdateServiceRequestDto.checkTattoo());
-        roleRepository.save(role);
+            tattoo
+        );
     }
-
+    @Override
+    @Transactional
     public void deleteRole(
-        Long jobPost_id,
-        Long role_id,
+        Long jobPostId,
+        Long roleId,
         final Account account
     ) {
-        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPost_id,
+        JobPost jobPost = jobPostRepository.findByIdAndCompany(jobPostId,
                 getCompanyByAccount(account))
             .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
-        Role role = roleRepository.findByIdAndJobPost(role_id, jobPost)
-            .orElseThrow(() -> new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
+        Role role = roleRepository.findByIdAndJobPost(roleId, jobPost)
+            .orElseThrow(() -> new RoleException(RoleErrorCode.NOT_FOUND_ROLE));
         roleRepository.delete(role);
     }
-
+    @Override
+    @Transactional(readOnly = true)
     public RoleServiceResponseDto readRole(
-        Long jobPost_id,
-        Long role_id
+        Long jobPostId,
+        Long roleId
     ) {
-        JobPost jobPost = jobPostRepository.findById(jobPost_id)
+        JobPost jobPost = jobPostRepository.findById(jobPostId)
             .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
-        Role role = roleRepository.findByIdAndJobPost(role_id, jobPost)
-            .orElseThrow(() -> new NotFoundRoleException(RoleErrorCode.NOT_FOUND_ROLE));
+        Role role = roleRepository.findByIdAndJobPost(roleId, jobPost)
+            .orElseThrow(() -> new RoleException(RoleErrorCode.NOT_FOUND_ROLE));
         int minAge = AgeCalculator(LocalDate.now(),role.getMinAge());
         int maxAge = AgeCalculator(LocalDate.now(),role.getMaxAge());
-        return roleEntityMapper.toRoleServiceResponseDto(role,minAge,maxAge);
+        return RoleServiceResponseDto.from(role,minAge,maxAge);
     }
-    private Integer AgeCalculator(LocalDate localDateNow,LocalDate localDate){
-        return Period.between(localDate,localDateNow).getYears();
-    }
-
-    public List<RoleServiceResponseDto> readAllRole(Long jobPost_id) {
-        JobPost jobPost = jobPostRepository.findById(jobPost_id)
+    @Override
+    @Transactional(readOnly = true)
+    public List<RoleServiceResponseDto> readAllRole(Long jobPostId) {
+        JobPost jobPost = jobPostRepository.findById(jobPostId)
             .orElseThrow(() -> new NotFoundJobPostException(JobPostErrorCode.NOT_FOUND_JOBPOST));
         return roleRepository.findAllByJobPost(jobPost)
             .stream()
             .map(role -> {
                 int minAge = AgeCalculator(LocalDate.now(), role.getMinAge());
                 int maxAge = AgeCalculator(LocalDate.now(), role.getMaxAge());
-                return roleEntityMapper.toRoleServiceResponseDto(role, minAge, maxAge);
+                return RoleServiceResponseDto.from(role,minAge,maxAge);
             })
             .toList();
+    }
+    private Integer AgeCalculator(LocalDate localDateNow,LocalDate localDate){
+        return Period.between(localDate,localDateNow).getYears();
+    }
+    private Company getCompanyByAccount(final Account account) {
+        return companyRepository.findByAccount(account)
+            .orElseThrow(() -> new AccountException(AccountErrorCode.NOT_FOUND_ACCOUNT));
     }
 }
