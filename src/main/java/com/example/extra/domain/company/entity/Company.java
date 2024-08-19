c0b8a8fbf156c920b33727ed1dccb74a5a39b407
shin0112 <wnslcosltimo12@gmail.com>
2024-08-20 01:08:17 +0900

    style: 불필요한 코드 삭제

--- src/main/java/com/example/extra/domain/company/entity/Company.java
+++ src/main/java/com/example/extra/domain/company/entity/Company.java
@@ -1,9 +1,7 @@
 package com.example.extra.domain.company.entity;
 
 import com.example.extra.domain.account.entity.Account;
-import com.example.extra.domain.jobpost.entity.JobPost;
 import com.example.extra.global.entity.BaseEntity;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,13 +9,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
