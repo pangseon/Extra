bf2964edef609ea8a76cce5bd3fcb80c36f9b913
hyeneung <imasaru22@gmail.com>
2024-08-19 20:18:48 +0900

    feature: dto validation

--- src/main/java/com/example/extra/domain/applicationrequest/dto/controller/ApplicationRequestMemberUpdateControllerRequestDto.java
+++ src/main/java/com/example/extra/domain/applicationrequest/dto/controller/ApplicationRequestMemberUpdateControllerRequestDto.java
@@ -4,7 +4,7 @@ import com.example.extra.global.enums.ApplyStatus;
 import jakarta.validation.constraints.NotNull;
 
 public record ApplicationRequestMemberUpdateControllerRequestDto(
-    @NotNull(message = "유효한 apply status를 입력해주세요.")
+    @NotNull(message = "유효한 applyStatus 값을 입력해주세요. 가능한 값: 'APPLIED', 'REJECTED', 'APPROVED'")
     ApplyStatus applyStatus
 ) {
 
