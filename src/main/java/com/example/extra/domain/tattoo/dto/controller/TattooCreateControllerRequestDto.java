2f9393b1d33eed503e321d4fc00527efd5d7f115
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 23:59:24 +0900

    style: 타투 dto 수정
    - swagger 기본값 false로 지정
    - 코드 포맷

--- src/main/java/com/example/extra/domain/tattoo/dto/controller/TattooCreateControllerRequestDto.java
+++ src/main/java/com/example/extra/domain/tattoo/dto/controller/TattooCreateControllerRequestDto.java
@@ -1,13 +1,23 @@
 package com.example.extra.domain.tattoo.dto.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record TattooCreateControllerRequestDto(
+    @Schema(defaultValue = "false")
     Boolean face,
+    @Schema(defaultValue = "false")
     Boolean chest,
+    @Schema(defaultValue = "false")
     Boolean arm,
+    @Schema(defaultValue = "false")
     Boolean leg,
+    @Schema(defaultValue = "false")
     Boolean shoulder,
+    @Schema(defaultValue = "false")
     Boolean back,
+    @Schema(defaultValue = "false")
     Boolean hand,
+    @Schema(defaultValue = "false")
     Boolean feet
 ) {
 
--- src/main/java/com/example/extra/domain/tattoo/dto/service/response/TattooReadServiceResponseDto.java
+++ src/main/java/com/example/extra/domain/tattoo/dto/service/response/TattooReadServiceResponseDto.java
@@ -1,29 +1,39 @@
 package com.example.extra.domain.tattoo.dto.service.response;
 
 import com.example.extra.domain.tattoo.entity.Tattoo;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record TattooReadServiceResponseDto(
+    @Schema(defaultValue = "false")
     Boolean face,
+    @Schema(defaultValue = "false")
     Boolean chest,
+    @Schema(defaultValue = "false")
     Boolean arm,
+    @Schema(defaultValue = "false")
     Boolean leg,
+    @Schema(defaultValue = "false")
     Boolean shoulder,
+    @Schema(defaultValue = "false")
     Boolean back,
+    @Schema(defaultValue = "false")
     Boolean hand,
+    @Schema(defaultValue = "false")
     Boolean feet
 ) {
-    public static TattooReadServiceResponseDto from(Tattoo tattoo){
+
+    public static TattooReadServiceResponseDto from(Tattoo tattoo) {
         return TattooReadServiceResponseDto.builder()
-                .face(tattoo.getFace())
-                .chest(tattoo.getChest())
-                .arm(tattoo.getArm())
-                .leg(tattoo.getLeg())
-                .shoulder(tattoo.getShoulder())
-                .back(tattoo.getBack())
-                .hand(tattoo.getHand())
-                .feet(tattoo.getFeet())
+            .face(tattoo.getFace())
+            .chest(tattoo.getChest())
+            .arm(tattoo.getArm())
+            .leg(tattoo.getLeg())
+            .shoulder(tattoo.getShoulder())
+            .back(tattoo.getBack())
+            .hand(tattoo.getHand())
+            .feet(tattoo.getFeet())
             .build();
     }
 }
