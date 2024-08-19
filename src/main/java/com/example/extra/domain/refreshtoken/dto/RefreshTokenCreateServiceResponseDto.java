b75bc8340be313bec7f70427b1f7596264bf04c7
shin0112 <wnslcosltimo12@gmail.com>
2024-08-19 16:27:30 +0900

    feature: 토큰 response dto swagger 정보 입력

--- src/main/java/com/example/extra/domain/refreshtoken/dto/RefreshTokenCreateServiceResponseDto.java
+++ src/main/java/com/example/extra/domain/refreshtoken/dto/RefreshTokenCreateServiceResponseDto.java
@@ -1,6 +1,9 @@
 package com.example.extra.domain.refreshtoken.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record RefreshTokenCreateServiceResponseDto(
+    @Schema(description = "리프레시 토큰", example = "Bearer token")
     String refreshToken
 ) {
 
