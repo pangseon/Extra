8bc72c554211ef7d33c973403b9b3c9fbee00d34
shin0112 <wnslcosltimo12@gmail.com>
2024-08-20 00:48:23 +0900

    feature: size 유효성 검사 추가

--- src/main/java/com/example/extra/domain/company/dto/controller/CompanyCreateControllerRequestDto.java
+++ src/main/java/com/example/extra/domain/company/dto/controller/CompanyCreateControllerRequestDto.java
@@ -2,6 +2,7 @@ package com.example.extra.domain.company.dto.controller;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CompanyCreateControllerRequestDto(
     @Schema(description = "계정 id")
@@ -9,6 +10,7 @@ public record CompanyCreateControllerRequestDto(
     Long accountId,
     @Schema(description = "업체 이름", example = "엑스트라")
     @NotBlank(message = "업체 이름은 필수 입력 정보입니다")
+    @Size(max = 30)
     String name
 ) {
 
