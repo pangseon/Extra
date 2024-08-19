16852149d61f7f446efa5cb5c17c5bcb5e3f2f0a
hyeneung <imasaru22@gmail.com>
2024-08-19 20:26:25 +0900

    feature: 의상 승인 게시판 및 역할 생성 관련 dto 검증

--- src/main/java/com/example/extra/domain/costumeapprovalboard/dto/controller/CostumeApprovalBoardExplainCreateRequestDto.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/dto/controller/CostumeApprovalBoardExplainCreateRequestDto.java
@@ -1,9 +1,11 @@
 package com.example.extra.domain.costumeapprovalboard.dto.controller;
 
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record CostumeApprovalBoardExplainCreateRequestDto(
+    @Size(max = 255, message = "imageExplain은 최대 255자입니다.")
     String imageExplain
 ) {
 
--- src/main/java/com/example/extra/domain/costumeapprovalboard/dto/controller/CostumeApprovalBoardExplainUpdateControllerRequestDto.java
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/dto/controller/CostumeApprovalBoardExplainUpdateControllerRequestDto.java
@@ -1,7 +1,12 @@
 package com.example.extra.domain.costumeapprovalboard.dto.controller;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CostumeApprovalBoardExplainUpdateControllerRequestDto(
+    @Size(max = 255, message = "imageExplain은 최대 255자입니다.")
     String imageExplain,
+    @NotNull(message = "imageChange는 필수 정보 입니다.")
     Boolean imageChange
 ) {
 
