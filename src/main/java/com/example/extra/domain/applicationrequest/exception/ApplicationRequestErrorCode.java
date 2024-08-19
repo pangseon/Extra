2db6a6e5b9106ce1da25c0ed1b66435361274491
hyeneung <imasaru22@gmail.com>
2024-08-19 15:15:05 +0900

    feature: 기존 지원한 공고와 촬영 날짜 겹치는데 지원 시 발생할 예외 코드 정의

--- src/main/java/com/example/extra/domain/applicationrequest/exception/ApplicationRequestErrorCode.java
+++ src/main/java/com/example/extra/domain/applicationrequest/exception/ApplicationRequestErrorCode.java
@@ -10,9 +10,11 @@ import org.springframework.http.HttpStatus;
 public enum ApplicationRequestErrorCode implements ErrorCode {
     // 400
     NOT_ABLE_TO_CANCEL_IN_APPROVED(HttpStatus.BAD_REQUEST, "승인 상태에서는 지원 철회가 불가합니다."),
-    NOT_ABLE_TO_APPLY_TO_JOB_POST(HttpStatus.BAD_REQUEST, "해당 공고는 마감되었습니다."),
+    NOT_ABLE_TO_APPLY_TO_JOB_POST_DUE_TO_STATUS(HttpStatus.BAD_REQUEST, "해당 공고는 마감되었습니다."),
+    NOT_ABLE_TO_APPLY_TO_JOB_POST_DUE_TO_DATE(HttpStatus.BAD_REQUEST, "지원한 다른 공고와 촬영 날짜가 겹칩니다."),
+    ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 해당 공고에 지원하였습니다."),
+
     NOT_APPROVED_REQUEST(HttpStatus.BAD_REQUEST, "승인되지 않은 공고입니다."),
-    ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 해당 역할에 지원하였습니다."),
 
     // 403
     NOT_ABLE_TO_ACCESS_APPLICATION_REQUEST_MEMBER(HttpStatus.FORBIDDEN, "접근 권한이 없습니다"),
