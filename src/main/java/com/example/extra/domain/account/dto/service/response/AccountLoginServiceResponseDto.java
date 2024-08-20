4f5a17da4e32865c7f750201077a6c8d690dc06f
shin0112 <wnslcosltimo12@gmail.com>
2024-08-20 12:33:00 +0900

    refactor: login response dto에 로그인 여부 확인하는 필드 추가

--- src/main/java/com/example/extra/domain/account/dto/service/response/AccountLoginServiceResponseDto.java
+++ src/main/java/com/example/extra/domain/account/dto/service/response/AccountLoginServiceResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.extra.domain.account.dto.service.response;
 
 public record AccountLoginServiceResponseDto(
+    boolean isLogin,
+    Long accountId,
     String accessToken,
     String refreshToken
 ) {
