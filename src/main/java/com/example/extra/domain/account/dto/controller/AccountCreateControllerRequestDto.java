0c73aadf8d82aebb2ab7b514278055eb87468999
shin0112 <wnslcosltimo12@gmail.com>
2024-08-20 12:37:12 +0900

    style: NotNull -> NotBlank

--- src/main/java/com/example/extra/domain/account/dto/controller/AccountCreateControllerRequestDto.java
+++ src/main/java/com/example/extra/domain/account/dto/controller/AccountCreateControllerRequestDto.java
@@ -1,23 +1,23 @@
 package com.example.extra.domain.account.dto.controller;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 @Schema(description = "회원 가입 작성 DTO")
 public record AccountCreateControllerRequestDto(
     @Schema(description = "이메일", example = "user@naver.com")
-    @NotNull(message = "이메일은 필수 항목입니다")
+    @NotBlank(message = "이메일은 필수 항목입니다")
     @Pattern(
         regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
         message = "이메일의 형식을 지켜주세요"
     )
     String email,
     @Schema(description = "비밀번호")
-    @NotNull(message = "비밀번호는 필수 항목입니다")
+    @NotBlank(message = "비밀번호는 필수 항목입니다")
     String password,
     @Schema(description = "사용자 역할")
-    @NotNull(message = "사용자 역할은 필수 항목입니다")
+    @NotBlank(message = "사용자 역할은 필수 항목입니다")
     @Pattern(
         regexp = "^(USER|COMPANY|ADMIN)$",
         message = "USER, COMPANY, ADMIN 중 하나를 입력해주세요"
