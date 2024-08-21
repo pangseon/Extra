8bb8ee5a351088ea67307c78c6de880216a8cd30
hyeneung <imasaru22@gmail.com>
2024-08-22 01:10:04 +0900

    feature: dto 정의

---
+++ src/main/java/com/example/extra/domain/costumeapprovalboard/dto/service/response/CostumeApprovalBoardMemberReadUrlServiceResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.extra.domain.costumeapprovalboard.dto.service.response;
+
+import lombok.Builder;
+
+@Builder
+public record CostumeApprovalBoardMemberReadUrlServiceResponseDto(
+    String imageUrl
+) {
+
+}
